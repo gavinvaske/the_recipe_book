9bc5dbdde4aed475b9634ff0b4136d7b60c7faea
Gavin Vaske <Gavin.Vaske@gmail.com>
2022-02-09 19:18:26 -0600

    Remove unused file

--- application/controllers/board.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/move', (request, response) => {
-    response.send('You moved');
-});
-
-module.exports = router;

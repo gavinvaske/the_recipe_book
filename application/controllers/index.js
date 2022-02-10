46d5fec63109755b16df465fcf622b58447a4aff
Gavin Vaske <Gavin.Vaske@gmail.com>
2022-02-09 19:19:49 -0600

    configure ejs and express-ejs-layouts; add homepage, login, register views and endpoints

--- application/controllers/index.js
+++ application/controllers/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (request, response) => {
-    response.send('Hello Mars!');
+    response.render('homepage');
 });
 
 module.exports = router;

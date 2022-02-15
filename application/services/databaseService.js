a5570ee32bdd14dca2d3abb320f2723967be65f5
Gavin Vaske <Gavin.Vaske@gmail.com>
2022-02-15 16:58:46 -0600

    add dependency mongodb-memory-server for testing; refactor functions to be async

--- application/services/databaseService.js
+++ application/services/databaseService.js
@@ -1,12 +1,35 @@
 const mongoose = require('mongoose');
+const {MongoMemoryServer} = require('mongodb-memory-server');
 
-exports.connectToMongooseDatabase = (databaseUrl) => {
+const mongod = new MongoMemoryServer();
+
+module.exports.connectToMongoDatabase = async (databaseUrl) => {
     if (!databaseUrl) {
         throw new Error('Database URL is not defined');
     }
 
-    mongoose.connect(databaseUrl, {
+    await mongoose.connect(databaseUrl, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
+}
+
+module.exports.connectToTestMongoDatabase = async () => {
+    const testDatabaseUrl = mongod.getUri();
+
+    await connectToMongoDatabase(testDatabaseUrl);
+}
+
+module.exports.closeDatabase = async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    await mongod.stop();
+}
+
+module.exports.clearDatabase = async () => {
+    const collections = mongoose.connection.collections;
+    for (const key in collections) {
+        const collection = collections[key];
+        await collection.deleteMany();
+    }
 }
--- test/services/databaseService.spec.js
+++ test/services/databaseService.spec.js
@@ -1,18 +1,16 @@
-const {connectToMongooseDatabase} = require('../../application/services/databaseService');
+const {connectToMongoDatabase} = require('../../application/services/databaseService');
 const chance = require('chance').Chance();
 const mongoose = require('mongoose');
 
 jest.mock('mongoose');
 
-it('should error if no database url is provided', () => {
-    expect(() => {
-        connectToMongooseDatabase(null)
-    }).toThrow('Database URL is not defined');
+it('should error if no database url is provided', async () => {
+    await expect(connectToMongoDatabase(null)).rejects.toThrow('Database URL is not defined')
 });
 
 it('should attempt to connect to mongo database', () => {
     const databaseUrl = chance.string();
-    connectToMongooseDatabase(databaseUrl);
+    connectToMongoDatabase(databaseUrl);
 
     expect(mongoose.connect).toHaveBeenCalledTimes(1);
     expect(mongoose.connect).toHaveBeenCalledWith(databaseUrl, {

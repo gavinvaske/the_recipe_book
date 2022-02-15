16428dda740d67e4c321684244a66c65341f7f5d
Gavin Vaske <Gavin.Vaske@gmail.com>
2022-02-15 16:59:28 -0600

    prefer module.exports over exports

--- application/services/userService.js
+++ application/services/userService.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
 
-exports.createUser = async (userAttributes) => {
+module.exports.createUser = async (userAttributes) => {
     const userModel = new User(userAttributes);
 
     return await userModel.save();

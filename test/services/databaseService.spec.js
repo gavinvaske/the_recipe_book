bd186af0f44590d1b18ee6ddcbe13c5d3a659c31
Gavin Vaske <Gavin.Vaske@gmail.com>
2022-02-15 16:08:21 -0600

    test mongoose database connection logic

--- test/services/databaseService.spec.js
+++ test/services/databaseService.spec.js
@@ -1,8 +1,22 @@
 const {connectToMongooseDatabase} = require('../../application/services/databaseService');
 const chance = require('chance').Chance();
+const mongoose = require('mongoose');
+
+jest.mock('mongoose');
 
 it('should error if no database url is provided', () => {
     expect(() => {
         connectToMongooseDatabase(null)
     }).toThrow('Database URL is not defined');
+});
+
+it('should attempt to connect to mongo database', () => {
+    const databaseUrl = chance.string();
+    connectToMongooseDatabase(databaseUrl);
+
+    expect(mongoose.connect).toHaveBeenCalledTimes(1);
+    expect(mongoose.connect).toHaveBeenCalledWith(databaseUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 });

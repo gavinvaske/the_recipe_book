4a5f936568b2d94d0f2b4f3b8897a4c44388b3e2
Gavin Vaske <Gavin.Vaske@gmail.com>
2022-03-02 20:03:58 -0600

    Wip: CRUD functionality for PRINTING_SETUP object

--- application/controllers/printingSetupController.js
+++ application/controllers/printingSetupController.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
+const {verifyJwtToken} = require('../middleware/authorize');
 
-router.get('/:id', (request, response) => {
-    response.render('viewPrintingSetups');
+router.get('/all/:recipeId', verifyJwtToken, (request, response) => {
+    return response.render('viewPrintingSetups', {
+        recipeId: request.params.recipeId
+    });
+});
+
+router.get('/create/:recipeId', verifyJwtToken, (request, response) => {
+    return response.render('createPrintingSetup', {
+        recipeId: request.params.recipeId,
+        user: request.user
+    });
+});
+
+router.post('/create', verifyJwtToken, (request, response) => {
+    response.send('TODO: POST /printing-setups/create');
 });
 
 module.exports = router;

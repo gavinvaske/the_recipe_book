2b4fadb2b778d90930c748143f3b0967a27bf5af
Storm Vaske <storm@labeladvantage.com>
2022-03-09 18:05:25 -0600

    ran through npm lint function and it fixed stuff now pushing up

--- application/public/js/main.js
+++ application/public/js/main.js
@@ -1,53 +1,48 @@
-function showPassword() {
-    let x = $(this).attr("class");
-    alert(x);
-}
-
 $( document ).ready(function() {
 
-$('.show-password-1').on('click', function(){
-    var passInput=$("#password");
-    if(passInput.attr('type')==='password')
-      {
-        passInput.attr('type','text');
-    }else{
-       passInput.attr('type','password');
-    }
-})
-
-$('.show-password-2').on('click', function(){
-    var passInput=$("#repeatPassword");
-    if(passInput.attr('type')==='password')
-      {
-        passInput.attr('type','text');
-    }else{
-       passInput.attr('type','password');
-    }
-})
-
-$('#password').on('click', function(){
-    $(this).next('.show-password-frame').addClass('active');
-})
-
-$('#repeatPassword').on('click', function(){
-    $(this).next('.show-password-frame').addClass('active');
-})
-
-
-$('#email').focusout(function(){
-    $('#email').filter(function(){
-      var email = $('#email').val();
-      var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-      if ( !emailReg.test( email ) ) {
-        $('.invalid-icon').addClass('active');
-        $('#email').addClass('invalid-email');
-        $('.text-danger').addClass('active');
-      } else {
-        $('.invalid-icon').removeClass('active');
-        $('#email').removeClass('invalid-email');
-        $('.text-danger').removeClass('active');
-      }
+    $('.show-password-1').on('click', function(){
+        var passInput=$('#password');
+        if (passInput.attr('type')==='password')
+        {
+            passInput.attr('type','text');
+        } else {
+            passInput.attr('type','password');
+        }
     });
-  });
+
+    $('.show-password-2').on('click', function(){
+        var passInput=$('#repeatPassword');
+        if (passInput.attr('type')==='password')
+        {
+            passInput.attr('type','text');
+        } else {
+            passInput.attr('type','password');
+        }
+    });
+
+    $('#password').on('click', function(){
+        $(this).next('.show-password-frame').addClass('active');
+    });
+
+    $('#repeatPassword').on('click', function(){
+        $(this).next('.show-password-frame').addClass('active');
+    });
+
+
+    $('#email').focusout(function(){
+        $('#email').filter(function(){
+            var email = $('#email').val();
+            var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+            if ( !emailReg.test( email ) ) {
+                $('.invalid-icon').addClass('active');
+                $('#email').addClass('invalid-email');
+                $('.text-danger').addClass('active');
+            } else {
+                $('.invalid-icon').removeClass('active');
+                $('#email').removeClass('invalid-email');
+                $('.text-danger').removeClass('active');
+            }
+        });
+    });
 
 });

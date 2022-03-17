4127a9759a6033bb53470d6d90a9adaaaed39bb3
Gavin Vaske <Gavin.Vaske@gmail.com>
2022-03-17 18:50:21 -0500

    revert fancy features that are not needed

--- application/middleware/authorize.js
+++ application/middleware/authorize.js
@@ -1,11 +1,10 @@
 const jwt = require('jsonwebtoken');
-const UserModel = require('../models/user');
 
 require('dotenv').config();
 
 const FORBIDDEN_STATUS_CODE = 403;
 
-module.exports.verifyJwtToken = async (request, response, next) => {
+module.exports.verifyJwtToken = (request, response, next) => {
     const token = request.cookies.jwtToken;
 
     if (!token) {
@@ -14,14 +13,8 @@ module.exports.verifyJwtToken = async (request, response, next) => {
 
     try {
         request.user = jwt.verify(token, process.env.JWT_SECRET);
-
-        const userFromDatabase = await UserModel.findById(request.user.id);
-
-        request.user.profilePicture = userFromDatabase.profilePicture;
-
         return next();
     } catch (error) {
-        request.flash('errors', ['Authorization could not be verified, please sign in again']);
         response.clearCookie('jwtToken');
         return response.redirect('/');
     }

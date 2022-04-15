1637e0fe9ff641717d6d6b303c2306ccb10c7e3a
Gavin Vaske <Gavin.Vaske@gmail.com>
2022-04-15 11:54:29 -0500

    define new materialOrder database object and test

---
+++ application/models/materialOrder.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    material: {
+        type: Schema.Types.ObjectId,
+        ref: 'Material',
+        required: true
+    },
+    purchaseOrderNumber: {
+        type: Number,
+        required: true,
+        validate : {
+            validator : Number.isInteger,
+            message   : '{VALUE} is not an integer'
+        }
+    },
+    orderDate: {
+        type: Date,
+        required: true
+    },
+    arrivalDate: {
+        type: Date,
+        required: true
+    },
+    feetPerRoll: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    totalRolls: {
+        type: Number,
+        required: true,
+        min: [1, 'Total Rolls cannot be less than 1'],
+        max: [100, 'Total Rolls cannot be greater than 100'],
+        validate : {
+            validator : Number.isInteger,
+            message   : '{VALUE} is not an integer'
+        }
+    },
+    totalCost: {
+        type: Number,
+        required: true,
+        min: [1, 'Total Cost must be more than $1'],
+        max: [500000, 'Total Cost must be less than $500,000']
+    },
+    vendor: {
+        type: Schema.Types.ObjectId,
+        ref: 'Vendor',
+        required: true
+    },
+    hasArrived: {
+        type: Boolean,
+        required: false
+    }
+}, { timestamps: true });
+
+const MaterialOrder = mongoose.model('materialOrders', schema);
+
+module.exports = MaterialOrder;
---
+++ test/models/materialOrder.spec.js
@@ -0,0 +1,263 @@
+const chance = require('chance').Chance();
+const MaterialOrderModel = require('../../application/models/materialOrder');
+const mongoose = require('mongoose');
+
+TOTAL_ROLLS_MIN = 1;
+TOTAL_ROLLS_MAX = 100;
+
+TOTAL_COST_MIN = 1;
+TOTAL_COST_MAX = 500000;
+
+describe('materialOrder validation', () => {
+    let materialOrderAttributes;
+
+    beforeEach(() => {
+        materialOrderAttributes = {
+            material: new mongoose.Types.ObjectId(),
+            purchaseOrderNumber: chance.integer(),
+            orderDate: chance.date({string: true}),
+            arrivalDate: chance.date({string: true}),
+            feetPerRoll: chance.integer({min: 0}),
+            totalRolls: chance.integer({min: TOTAL_ROLLS_MIN, max: TOTAL_ROLLS_MAX}),
+            totalCost: chance.floating({min: TOTAL_COST_MIN, max: TOTAL_COST_MAX}),
+            vendor: new mongoose.Types.ObjectId(),
+            hasArrived: chance.bool()
+        };
+    });
+
+    it('should validate when all attributes are defined correctly', () => {
+        const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+    
+        const error = materialOrder.validateSync();
+
+        expect(error).toBe(undefined);
+    });
+
+    describe('materialOrder.material validation', () => {
+        it('should fail validation if material is not defined', () => {
+            delete materialOrderAttributes.material;
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).not.toBe(undefined);
+        })
+    });
+
+    describe('materialOrder.purchaseOrderNumber validation', () => {
+        it('should fail validation if purchaseOrderNumber is not defined', () => {
+            delete materialOrderAttributes.purchaseOrderNumber;
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).not.toBe(undefined);
+        });
+
+        it('should fail validation if purchaseOrderNumber is not an integer', () => {
+            materialOrderAttributes.purchaseOrderNumber = chance.floating()
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).not.toBe(undefined);
+        });
+
+        it('should fail validation if purchaseOrderNumber contains non-numberic characters', () => {
+            materialOrderAttributes.purchaseOrderNumber = `${chance.integer()}${chance.word()}`
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).not.toBe(undefined);
+        });
+    });
+
+    describe('materialOrder.orderDate validation', () => {
+        it('should fail validation if orderDate is not defined', () => {
+            delete materialOrderAttributes.orderDate;
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).not.toBe(undefined);
+        });
+
+        it('should fail validation if orderDate is not a date', () => {
+            materialOrderAttributes.orderDate = chance.word();
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).not.toBe(undefined);
+        });
+    });
+
+    describe('materialOrder.arrivalDate validation', () => {
+        it('should fail validation if arrivalDate is not defined', () => {
+            delete materialOrderAttributes.arrivalDate;
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).not.toBe(undefined);
+        });
+
+        it('should fail validation if arrivalDate is not a date', () => {
+            materialOrderAttributes.arrivalDate = chance.word();
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).not.toBe(undefined);
+        });
+    });
+
+    describe('materialOrder.feetPerRoll validation', () => {
+        it('should fail validation if feetPerRoll is not defined', () => {
+            delete materialOrderAttributes.feetPerRoll;
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).not.toBe(undefined);
+        });
+
+        it('should validate if feetPerRoll is a non-negative floating value', () => {
+            materialOrderAttributes.feetPerRoll = chance.floating({min: 0, max: 10000});
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).toBe(undefined);
+        })
+        
+        it('should fail validation if feetPerRoll is less than 0', () => {
+            materialOrderAttributes.feetPerRoll = chance.floating({min: -10000, max: 0});
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).not.toBe(undefined);
+        })
+    });
+
+    describe('materialOrder.totalRolls validation', () => {
+        it('should fail validation if totalRolls is not defined', () => {
+            delete materialOrderAttributes.totalRolls;
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).not.toBe(undefined);
+        });
+
+        it('should fail validation if totalRolls less than 1', () => {
+            materialOrderAttributes.totalRolls = chance.integer({max: TOTAL_ROLLS_MIN - 1});
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).not.toBe(undefined);
+        });
+
+        it('should fail validation if totalRolls greater than 100', () => {
+            materialOrderAttributes.totalRolls = chance.integer({min: TOTAL_ROLLS_MAX + 1});
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).not.toBe(undefined);
+        });
+
+        it('should fail validation if totalRolls is not an integer', () => {
+            materialOrderAttributes.totalRolls = chance.floating({min: TOTAL_ROLLS_MIN, max: TOTAL_ROLLS_MAX});
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).not.toBe(undefined);
+        });
+    });
+    describe('materialOrder.totalCost validation', () => {
+        it('should fail validation if totalCost is not defined', () => {
+            delete materialOrderAttributes.totalCost;
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).not.toBe(undefined);
+        });
+
+        it('should fail validation if totalCost less than 1', () => {
+            materialOrderAttributes.totalCost = chance.floating({max: TOTAL_COST_MIN - 1});
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).not.toBe(undefined);
+        });
+
+        it('should fail validation if totalRolls greater than 500,000', () => {
+            materialOrderAttributes.totalCost = chance.floating({min: TOTAL_COST_MAX + 1});
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).not.toBe(undefined);
+        });
+    });
+
+    describe('materialOrder.vendor validation', () => {
+        it('should fail validation if vendor is not defined', () => {
+            delete materialOrderAttributes.vendor;
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).not.toBe(undefined);
+        });
+
+        it('should fail validation if vendor not the correct type', () => {
+            materialOrderAttributes.vendor = chance.string();
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).not.toBe(undefined);
+        });
+    });
+
+    describe('materialOrder.hasArrived validation', () => {
+        it('should NOT fail validation if hasArrived is not defined', () => {
+            delete materialOrderAttributes.hasArrived;
+
+            const materialOrder = new MaterialOrderModel(materialOrderAttributes);
+
+            const error = materialOrder.validateSync();
+
+            expect(error).toBe(undefined);
+        });
+    });
+    
+})

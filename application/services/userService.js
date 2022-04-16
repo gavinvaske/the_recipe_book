19afb175f1e167cc1aed4ca381c7cad03c8869e0
Gavin Vaske <Gavin.Vaske@gmail.com>
2022-04-16 13:46:46 -0500

    Feature: if user is already logged in, redirect immediately away from login page

--- application/services/userService.js
+++ application/services/userService.js
@@ -1,7 +1,24 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 module.exports.createUser = async (userAttributes) => {
     const userModel = new User(userAttributes);
 
     return await userModel.save();
+};
+
+module.exports.isUserLoggedIn = (jwtToken, jwtSecret) => {
+    if (!jwtToken) {
+        return false;
+    }
+
+    try {
+        const resp = jwt.verify(jwtToken, jwtSecret);
+        console.log(`here is the jwt.verify response => ${JSON.stringify(resp)}`);
+        return true;
+    } catch (error) {
+        console.log(`error during isJwtTokenValid(): ${JSON.stringify(error)}`);
+    }
+
+    return false;
 };

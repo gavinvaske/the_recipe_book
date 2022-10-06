5a060a981508c94830b47a0d669c15d5c3bd6ecb
Gavin Vaske <Gavin.Vaske@gmail.com>
2022-10-05 21:27:24 -0500

    finishing testing materialInventoryService.js

--- test/services/materialInventoryService.spec.js
+++ test/services/materialInventoryService.spec.js
@@ -1,19 +1,25 @@
 const materialInventoryService = require('../../application/services/materialInventoryService');
 const mockMaterialModel = require('../../application/models/material');
+const mockMaterialOrderService = require('../../application/services/materialOrderService');
+const chance = require('chance').Chance();
 
 jest.mock('../../application/models/material');
+jest.mock('../../application/services/materialOrderService');
 
 describe('materialInventoryService test suite', () => {
     let execFunction,
         findFunction, 
-        leanFunction;
+        leanFunction,
+        materialsInDatabase;
 
-    afterEach(() => {
-        jest.resetAllMocks();
-    });
-
-    beforeEach(() => {
-        execFunction = jest.fn();
+        
+        afterEach(() => {
+            jest.resetAllMocks();
+        });
+        
+        beforeEach(() => {
+        materialsInDatabase = [];
+        execFunction = jest.fn().mockResolvedValue(materialsInDatabase);
         leanFunction = jest.fn().mockImplementation(() => {
             return {
                 exec: execFunction
@@ -33,12 +39,47 @@ describe('materialInventoryService test suite', () => {
             await expect(materialInventoryService.getAllMaterialInventoryData()).resolves.not.toThrowError();
         });        
         
-        it ('should not throw error', async () => {
+        it ('should search for materials from database', async () => {
             await materialInventoryService.getAllMaterialInventoryData();
 
             expect(findFunction).toHaveBeenCalledTimes(1);
             expect(leanFunction).toHaveBeenCalledTimes(1);
             expect(execFunction).toHaveBeenCalledTimes(1);
         });
+
+        it ('should return empty array when no materials are found by query', async () => {
+            materialsInDatabase = [];
+        
+            const materials = await materialInventoryService.getAllMaterialInventoryData();
+
+            expect(materials).toBeDefined();
+            expect(materials.length).toBe(0);
+        });
+
+        it ('should return correct data', async () => {
+            materialsInDatabase = [
+                {
+                    [chance.string()]: chance.string()
+                }
+            ];
+            const expectedLengthOfMaterialOrdered = 87;
+            const expectedLengthOfMaterialInInventory = 23;
+            execFunction = jest.fn().mockResolvedValue(materialsInDatabase);
+
+            mockMaterialOrderService.getLengthOfOneMaterialOrdered.mockReturnValue(expectedLengthOfMaterialOrdered)
+            mockMaterialOrderService.getLengthOfOneMaterialInInventory.mockReturnValue(expectedLengthOfMaterialInInventory)
+        
+            const materials = await materialInventoryService.getAllMaterialInventoryData();
+
+            expect(materials).toBeDefined();
+            expect(materials.length).toBe(1);
+            expect(materials).toEqual([
+                {
+                    ...materialsInDatabase[0],
+                    lengthOfMaterialOrdered: expectedLengthOfMaterialOrdered,
+                    lengthOfMaterialInStock: expectedLengthOfMaterialInInventory
+            }
+        ]);
+        })
     })
 });

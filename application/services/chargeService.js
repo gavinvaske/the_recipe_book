26148a51cd3b080321d983a58aeee92838c334bb
Gavin Vaske <Gavin.Vaske@gmail.com>
2022-10-26 17:02:51 -0500

    fix issue where uploaded XML docs did not have charge items removed properly

--- application/services/chargeService.js
+++ application/services/chargeService.js
@@ -1,1 +1,1 @@
-module.exports.PRODUCT_NUMBER_IS_FOR_AN_EXTRA_CHARGE = /(.*)-([a-z]|[A-Z])$/;
+module.exports.PRODUCT_NUMBER_IS_FOR_AN_EXTRA_CHARGE = /(.*)-([a-z]|[A-Z])*$/;
---
+++ test/services/chargeService.spec.js
@@ -0,0 +1,46 @@
+const chance = require('chance').Chance();
+const {PRODUCT_NUMBER_IS_FOR_AN_EXTRA_CHARGE} = require('../../application/services/chargeService');
+
+describe('chargeService test suite', () => {
+    let productNumber;
+
+    it('should NOT return a regex match', () => {
+        productNumber = chance.word() + `-${chance.integer({min: 0, max: 1000})}`;
+
+        const isAMatch = PRODUCT_NUMBER_IS_FOR_AN_EXTRA_CHARGE.test(productNumber);
+
+        expect(isAMatch).toBe(false);
+    });
+
+    it('should NOT return a regex match', () => {
+        productNumber = chance.word();
+
+        const isAMatch = PRODUCT_NUMBER_IS_FOR_AN_EXTRA_CHARGE.test(productNumber);
+
+        expect(isAMatch).toBe(false);
+    });
+
+    it('should NOT return a regex match', () => {
+        productNumber = chance.word() + '-' + chance.integer({min: 0, max: 1000});
+
+        const isAMatch = PRODUCT_NUMBER_IS_FOR_AN_EXTRA_CHARGE.test(productNumber);
+
+        expect(isAMatch).toBe(false);
+    });
+
+    it('should return a regex match', () => {
+        productNumber = chance.word() + '-' + chance.word();
+
+        const isAMatch = PRODUCT_NUMBER_IS_FOR_AN_EXTRA_CHARGE.test(productNumber);
+
+        expect(isAMatch).toBe(true);
+    });
+
+    it('should NOT return a regex match', () => {
+        productNumber = chance.word() + '-' + chance.word() + '1234';
+
+        const isAMatch = PRODUCT_NUMBER_IS_FOR_AN_EXTRA_CHARGE.test(productNumber);
+
+        expect(isAMatch).toBe(false);
+    });
+});

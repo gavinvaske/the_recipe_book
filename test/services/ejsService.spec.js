67bfe2397579922156c22504e322ee58e72520a1
Gavin Vaske <Gavin.Vaske@gmail.com>
2022-11-08 20:37:16 -0600

    add test cases for ejsService

---
+++ test/services/ejsService.spec.js
@@ -0,0 +1,27 @@
+const ejsService = require('../../application/services/ejsService');
+
+describe('ejsService test suite', () => {
+    it('should have a method named prettifyDuration()', () => {
+        expect(ejsService.prettifyDuration).toBeDefined();
+    });
+    
+    it('should have a method named getSimpleDate()', () => {
+        expect(ejsService.getSimpleDate).toBeDefined();
+    });
+
+    it('should have a method named getOverallTicketDuration()', () => {
+        expect(ejsService.getOverallTicketDuration).toBeDefined();
+    });
+
+    it('should have a method named getHowLongTicketHasBeenInProduction()', () => {
+        expect(ejsService.getHowLongTicketHasBeenInProduction).toBeDefined();
+    });
+
+    it('should have a method named getHowLongTicketHasBeenInDepartment()', () => {
+        expect(ejsService.getHowLongTicketHasBeenInDepartment).toBeDefined();
+    });
+
+    it('should have a method named getHowLongTicketHasHadADepartmentStatus()', () => {
+        expect(ejsService.getHowLongTicketHasHadADepartmentStatus).toBeDefined();
+    });
+});

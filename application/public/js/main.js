e200c2520756bcbd4553c2866fa6ccc01b9a3879
Storm Vaske <storm@labeladvantage.com>
2022-11-10 06:11:42 -0600

    added data attribute selection for deparments when a user selects to move ticket to another department

--- application/public/js/main.js
+++ application/public/js/main.js
@@ -346,6 +346,7 @@ $( document ).ready(function() {
         if ($(this).find('.dropdown-menu').hasClass('active')){ 
             $('.dropdown-menu').removeClass('active');
         } else {
+            $('.full-page-curtain').removeClass('active');
             $('.dropdown-menu').removeClass('active');
             $(this).find('.dropdown-menu').addClass('active');
         }
@@ -600,6 +601,7 @@ $( document ).ready(function() {
     });
 
     $('.notification-option').click(function() {
+        $('')
         $(this).toggleClass('active');
     });
 
@@ -616,6 +618,7 @@ $( document ).ready(function() {
     });
 
     $('.settings-option.settings').click(function(){
+        $('.dropdown-menu').removeClass('active');
         $('.full-page-curtain').toggleClass('active');
     });
 
@@ -641,6 +644,12 @@ $( document ).ready(function() {
         } else {
             $('.sidebar-option ').removeClass('active');
         }
+    });
+
+    /* Gavin code for console.logs department seleciton */
+    $('.departments-dropdown li').click(function() {
+        let departmentSelection = $(this).data('department-name');
+        console.log(departmentSelection);
     });
 });
 

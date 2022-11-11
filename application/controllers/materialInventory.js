8134c8e3efd3c6a522357c8422ed4667cc424ed0
Gavin Vaske <Gavin.Vaske@gmail.com>
2022-11-11 14:58:54 -0600

    wip: improving performance on inventory page

--- application/controllers/materialInventory.js
+++ application/controllers/materialInventory.js
@@ -8,17 +8,30 @@ router.use(verifyJwtToken);
 
 router.get('/', async (request, response) => {
     try {
+        const start1 = new Date();
         const materialInventories = await materialInventoryService.getAllMaterialInventoryData();
+        const start2 = new Date();
         const lengthOfAllMaterialsInInventory = await materialOrderService.getLengthOfAllMaterialsInInventory();
+        const start3 = new Date();
         const lengthOfAllMaterialsOrdered = await materialOrderService.getLengthOfAllMaterialsOrdered();
+        const start4 = new Date();
         const totalPurchaseOrders = await materialOrderService.getNumberOfPurchaseOrders();
         
-        return response.render('viewMaterialInventory', {
-            materialInventories,
-            lengthOfAllMaterialsInInventory,
-            lengthOfAllMaterialsOrdered,
-            totalPurchaseOrders
-        });
+        const stop = new Date();
+
+        console.log(`Time Taken to execute start1 = ${(stop - start1)} ms`); // 1433 ms
+        console.log(`Time Taken to execute start2 = ${(stop - start2)} ms`); // 163 msg
+        console.log(`Time Taken to execute start3 = ${(stop - start3)} ms`); // 109 ms
+        console.log(`Time Taken to execute start4 = ${(stop - start4)} ms`); // 55 ms
+
+        return response.json('Performance upgrades incoming')
+
+        // return response.render('viewMaterialInventory', {
+        //     materialInventories,
+        //     lengthOfAllMaterialsInInventory,
+        //     lengthOfAllMaterialsOrdered,
+        //     totalPurchaseOrders
+        // });
 
     } catch (error) {
         console.log(`An error occurred while attempting to load /material-inventory: ${error}`);
--- application/services/materialInventoryService.js
+++ application/services/materialInventoryService.js
@@ -1,4 +1,5 @@
 const MaterialModel = require('../models/material');
+const PurchaseOrderModel = require('../models/materialOrder');
 const materialOrderService = require('../services/materialOrderService');
 
 module.exports.getAllMaterialInventoryData = async () => {
@@ -8,21 +9,81 @@ module.exports.getAllMaterialInventoryData = async () => {
         .lean()
         .exec();
 
-    for (let i = 0; i < materials.length; i++) {
-        const material = materials[i];
-        const materialId = material._id;
+    const materialIds = materials.map((material) => {
+        return material._id;
+    })
 
-        const lengthOfMaterialOrdered = await materialOrderService.getLengthOfOneMaterialOrdered(materialId);
-        const lengthOfMaterialInStock = await materialOrderService.getLengthOfOneMaterialInInventory(materialId);
-        const purchaseOrdersForMaterial = await materialOrderService.findPurchaseOrdersByMaterialThatHaveNotArrived(materialId);
+    console.log(`materials.length => ${materials.length}`)
+    console.log(`materialIds.length => ${materialIds.length}`)
 
-        materialInventories.push({
-            material,
-            lengthOfMaterialOrdered,
-            lengthOfMaterialInStock,
-            purchaseOrdersForMaterial
-        });
+    const searchQuery = {
+        material: {$in: materials}
     }
+    const purchaseOrders = await PurchaseOrderModel
+        .find(searchQuery)
+        .exec();
 
-    return materialInventories;
+    const materialIdToPurchaseOrders = {};
+
+    materialIds.forEach((materialId) => {
+        materialIdToPurchaseOrders[materialId] = [];
+    });
+
+    purchaseOrders.forEach((purchaseOrder) => {
+        const materialId = purchaseOrder.material;
+        
+        materialIdToPurchaseOrders[materialId].push(purchaseOrder);
+    });
+
+    console.log(`materialIdToPurchaseOrders => ${JSON.stringify(materialIdToPurchaseOrders)}`);
+
+    materials.forEach((material) => {
+        const materialInventory = buildMaterialInventory(material, materialIdToPurchaseOrders);
+
+
+    });
+
+    // return materialInventories;
 };
+
+function buildMaterialInventory(material, materialIdToPurchaseOrders) {
+    const materialId = material._id;
+    const lengthOfMaterialOrdered = 69;
+    const lengthOfMaterialInStock = 69;
+    const purchaseOrdersForMaterial = 69;
+
+    return {
+        material,
+        lengthOfMaterialOrdered,
+        lengthOfMaterialInStock,
+        purchaseOrdersForMaterial
+    }
+}
+
+// module.exports.getAllMaterialInventoryData = async () => {
+//     let materialInventories = [];
+//     const materials = await MaterialModel
+//         .find()
+//         .lean()
+//         .exec();
+
+//     console.log(`materials.length => ${materials.length}`);
+
+//     for (let i = 0; i < materials.length; i++) {
+//         const material = materials[i];
+//         const materialId = material._id;
+
+//         const lengthOfMaterialOrdered = await materialOrderService.getLengthOfOneMaterialOrdered(materialId);
+//         const lengthOfMaterialInStock = await materialOrderService.getLengthOfOneMaterialInInventory(materialId);
+//         const purchaseOrdersForMaterial = await materialOrderService.findPurchaseOrdersByMaterialThatHaveNotArrived(materialId);
+
+//         materialInventories.push({
+//             material,
+//             lengthOfMaterialOrdered,
+//             lengthOfMaterialInStock,
+//             purchaseOrdersForMaterial
+//         });
+//     }
+
+//     return materialInventories;
+// };

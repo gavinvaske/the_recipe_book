8e59944a5063d1b860464e75e4864281421dff55
Gavin Vaske <gavin.vaske@gmail.com>
2022-12-17 19:17:32 -0600

    write first cypress test; update .gitignore

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,4 +2,12 @@ describe('My First Test', () => {
   it('should never fail this sanity check', () => {
     expect(true).to.equal(true)
   });
+
+  it('Visits the Kitchen Sink', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('Visits the Kitchen Sink', () => {
+    cy.visit('http://localhost:8080/')
+  })
 });

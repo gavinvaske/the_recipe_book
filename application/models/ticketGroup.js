5efd7ffa316ccdfe829f7615c1b140456addfefb
Gavin Vaske <Gavin.Vaske@gmail.com>
2023-01-15 16:36:43 -0600

    fix issue where missing attribute defaulted to an emtpy array which broke validation

--- application/models/ticketGroup.js
+++ application/models/ticketGroup.js
@@ -9,6 +9,7 @@ const ticketGroupSchema = new Schema({
     ticketIdsInGroup: {
         type: [Schema.Types.ObjectId],
         ref: 'Ticket',
+        default: undefined,
         required: true,
         validate: [isArrayLengthGreaterThanZero, '"{PATH}" cannot be an empty array']
     }

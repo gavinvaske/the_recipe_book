6e4647db5dc2982fcb8d23524f6b13421ccf54d6
Gavin Vaske <Gavin.Vaske@gmail.com>
2023-02-04 19:16:07 -0600

    remove code comments

--- application/services/socketService.js
+++ application/services/socketService.js
@@ -73,49 +73,4 @@ module.exports = function(io){
             purchaseOrder: purchaseOrder
         });
     });
-
-    // PurchaseOrderModel.watch().on('change', async (change) => {
-    //     console.log(`Change to a PurchaseOrder in database: ${JSON.stringify(change)}`);
-
-    //     const purchaseOrder = await PurchaseOrderModel
-    //         .findById(change.documentKey._id)
-    //         .populate({path: 'material'})
-    //         .lean()
-    //         .exec();
-
-    //     if (!purchaseOrder) {
-    //         return;
-    //     }
-
-    //     const materialObjectId = purchaseOrder.material ? purchaseOrder.material._id : null;
-
-    //     if (!materialObjectId) {
-    //         return;
-    //     }
-
-    //     const allMaterials = await materialService.getAllMaterials();
-    //     const distinctMaterialIds = materialService.getMaterialIds(allMaterials);
-
-    //     const allPurchaseOrders = await purchaseOrderService.getPurchaseOrdersForMaterials(distinctMaterialIds);
-        
-    //     const materialIdToPurchaseOrders = materialInventoryService.mapMaterialIdToPurchaseOrders(distinctMaterialIds, allPurchaseOrders);
-
-    //     const allPurchaseOrdersForOneMaterial = materialIdToPurchaseOrders[materialObjectId];
-    //     const allPurchaseOrdersThatHaveArrived = purchaseOrderService.findPurchaseOrdersThatHaveArrived(allPurchaseOrders); 
-    //     const allPurchaseOrdersThatHaveNotArrived = purchaseOrderService.findPurchaseOrdersThatHaveNotArrived(allPurchaseOrders);
-
-    //     const materialInventory = materialInventoryService.buildMaterialInventory(purchaseOrder.material, allPurchaseOrdersForOneMaterial);
-    //     const lengthOfAllMaterialsInInventory = purchaseOrderService.computeLengthOfMaterial(allPurchaseOrdersThatHaveArrived);
-    //     const lengthOfAllMaterialsOrdered = purchaseOrderService.computeLengthOfMaterial(allPurchaseOrdersThatHaveNotArrived);
-    //     const totalPurchaseOrders = allPurchaseOrders.length;
-
-    //     io.emit(materialObjectId, {
-    //         lengthOfMaterialOrdered: materialInventory.lengthOfMaterialOrdered,
-    //         lengthOfMaterialInStock: materialInventory.lengthOfMaterialInStock,
-    //         lengthOfAllMaterialsInInventory: lengthOfAllMaterialsInInventory,
-    //         lengthOfAllMaterialsOrdered: lengthOfAllMaterialsOrdered,
-    //         totalPurchaseOrders: totalPurchaseOrders,
-    //         purchaseOrder: purchaseOrder
-    //     });
-    // });
 };

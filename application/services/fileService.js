67ec1d45db01bf1e0a3d0701dddafb0c045dc218
Gavin Vaske <Gavin.Vaske@gmail.com>
2023-02-25 17:06:38 -0600

    remove unused method

--- application/services/fileService.js
+++ application/services/fileService.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const fs = require('fs');
-const fileSchema = require('../schemas/s3File');
-const mongoose = require('mongoose');
 
 module.exports.getUploadedFilePath = (uploadedFileName) => {
     return path.join(path.resolve(__dirname, '../../') + '/uploads/' + uploadedFileName);
@@ -30,25 +28,6 @@ module.exports.deleteMultipleFilesFromFileSystem = (filePaths) => {
     filePaths.forEach((filePath) => {
         this.deleteOneFileFromFileSystem(filePath);
     });
-};
-
-module.exports.buildFiles = (fileNames, fileUrls) => {
-    if (fileNames.length !== fileUrls.length) {
-        throw new Error('"fileNames" must map one-to-one with "fileUrls"');
-    }
-
-    FileModel = mongoose.model('File', fileSchema);
-    const files = [];
-
-    for (let i = 0; i < fileNames.length; i++) {
-        const fileAttributes = {
-            fileName: fileNames[i],
-            url: fileUrls[i]
-        };
-
-        files.push(new FileModel(fileAttributes));
-    }
-    return files;
 };
 
 module.exports.getUploadedFileContents = (filePaths) => {

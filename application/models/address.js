f7ed7315a299ba0820c9a668472e3b49d40234f5
Gavin Vaske <Gavin.Vaske@gmail.com>
2023-07-08 11:51:59 -0500

    create address model that customer model will utilize

---
+++ application/models/address.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+mongoose.Schema.Types.String.set('trim', true);
+const Schema = mongoose.Schema;
+
+ZIP_CODE_REGEX = /(^\d{5}(?:[-\s]\d{4})?$)/
+
+function validateZipCode(zipCode) {
+  return ZIP_CODE_REGEX.test(zipCode)
+}
+
+const schema = new Schema({
+    name: {
+        type: String,
+        required: true, 
+    },
+    street: {
+      type: String,
+      uppercase: true,
+      required: true
+    },
+    unitOrSuite: {
+      type: String,
+      uppercase: true,
+      required: false
+    },
+    city: {
+      type: String,
+      uppercase: true,
+      required: true
+    },
+    state: {
+      type: String,
+      uppercase: true,
+      required: true
+    },
+    zipCode: {
+      type: String,
+      required: true,
+      validate: [validateZipCode, 'The provided zip code of "{VALUE}" is not a correctly formatted zip code']
+    },
+}, { timestamps: true });
+
+const Address = mongoose.model('Address', schema);
+
+module.exports = Address;
---
+++ test/models/address.spec.js
@@ -0,0 +1,187 @@
+const chance = require('chance').Chance();
+const Address = require('../../application/models/address');
+
+describe('validation', () => {
+    let addressAttributes;
+
+    beforeEach(() => {
+        addressAttributes = {
+          name: chance.string(),
+          street: chance.string(),
+          unitOrSuite: chance.string(),
+          city: chance.string(),
+          state: chance.string(),
+          zipCode: '80426',
+        }
+    });
+
+    it('should validate if all attributes are defined successfully', () => {
+        const address = new Address(addressAttributes);
+    
+        const error = address.validateSync();
+
+        expect(error).toBe(undefined);
+    });
+
+    describe('attribute: name', () => {
+      it('should fail validation if attribute is undefined', () => {
+        delete addressAttributes.name;
+        const address = new Address(addressAttributes);
+    
+        const error = address.validateSync();
+
+        expect(error).toBeDefined()
+      });
+
+      it('should be of type String', () => {
+        const address = new Address(addressAttributes);
+
+        expect(address.name).toEqual(expect.any(String));
+      });
+
+      it('should trim trailing or leading spaces', () => {
+        const nameWithoutSpaces = addressAttributes.name;
+        addressAttributes.name = '  ' + nameWithoutSpaces + ' ';
+        const address = new Address(addressAttributes);
+
+        expect(address.name).toEqual(nameWithoutSpaces);
+      });
+    });
+
+    describe('attribute: street', () => {
+      it('should NOT fail validation if attribute is undefined', () => {
+        delete addressAttributes.street;
+        const address = new Address(addressAttributes);
+    
+        const error = address.validateSync();
+
+        expect(error).toBeDefined()
+      });
+
+      it('should be of type String', () => {
+        const address = new Address(addressAttributes);
+
+        expect(address.street).toEqual(expect.any(String));
+      });
+
+      it('should convert to uppercase', () => {
+        const lowerCaseStreet = chance.string().toLowerCase();
+        addressAttributes.street = lowerCaseStreet;
+        const address = new Address(addressAttributes);
+
+        expect(address.street).toEqual(lowerCaseStreet.toUpperCase());
+      });
+    });
+
+    describe('attribute: unitOrSuite', () => {
+      it('should NOT fail validation if attribute is not defined', () => {
+        delete addressAttributes.unitOrSuite;
+        const address = new Address(addressAttributes);
+    
+        const error = address.validateSync();
+
+        expect(error).not.toBeDefined()
+      });
+
+      it('should be of type String', () => {
+        const address = new Address(addressAttributes);
+
+        expect(address.unitOrSuite).toEqual(expect.any(String));
+      });
+
+      it('should convert to uppercase', () => {
+        const lowerCaseUnitOrSuite = chance.string().toLowerCase();
+        addressAttributes.unitOrSuite = lowerCaseUnitOrSuite;
+        const address = new Address(addressAttributes);
+
+        expect(address.unitOrSuite).toEqual(lowerCaseUnitOrSuite.toUpperCase());
+      });
+    });
+
+    describe('attribute: city', () => {
+      it('should fail validation if attribute is not defined', () => {
+        delete addressAttributes.city;
+        const address = new Address(addressAttributes);
+    
+        const error = address.validateSync();
+
+        expect(error).toBeDefined();
+      });
+
+      it('should be of type String', () => {
+        const address = new Address(addressAttributes);
+
+        expect(address.city).toEqual(expect.any(String));
+      });
+
+      it('should convert to uppercase', () => {
+        const lowerCaseCity = chance.string().toLowerCase();
+        addressAttributes.city = lowerCaseCity;
+        const address = new Address(addressAttributes);
+
+        expect(address.city).toEqual(lowerCaseCity.toUpperCase());
+      });
+    });
+
+    describe('attribute: zipCode', () => {
+      it('should fail validation if attribute is not defined', () => {
+        delete addressAttributes.zipCode;
+        const address = new Address(addressAttributes);
+    
+        const error = address.validateSync();
+
+        expect(error).toBeDefined()
+      });
+
+      it('should be of type String', () => {
+        const address = new Address(addressAttributes);
+
+        expect(address.zipCode).toEqual(expect.any(String));
+      });
+
+      it('should fail validation if value is an incorrectly formatted zip code', () => {
+        const incorrectlyFormattedZipCodes = ['123', 'faerf', '5434-4324'];
+        addressAttributes.zipCode = chance.pickone(incorrectlyFormattedZipCodes);
+        const address = new Address(addressAttributes);
+    
+        const error = address.validateSync();
+
+        expect(error).toBeDefined()
+      });
+
+      it('should pass validation if value is a correctly formatted zip code', () => {
+        const correctZipCodes = ['80426', '16735-1309', '16735 1309'];
+        addressAttributes.zipCode = chance.pickone(correctZipCodes);
+        const address = new Address(addressAttributes);
+    
+        const error = address.validateSync();
+
+        expect(error).not.toBeDefined()
+      });
+    });
+
+    describe('attribute: state', () => {
+      it('should fail validation if attribute is not defined', () => {
+        delete addressAttributes.state;
+        const address = new Address(addressAttributes);
+    
+        const error = address.validateSync();
+
+        expect(error).toBeDefined()
+      });
+
+      it('should convert to uppercase', () => {
+        const lowerCaseState = chance.string().toLowerCase();
+        addressAttributes.state = lowerCaseState;
+        const address = new Address(addressAttributes);
+
+        expect(address.state).toEqual(lowerCaseState.toUpperCase());
+      });
+
+      it('should be of type String', () => {
+        const address = new Address(addressAttributes);
+
+        expect(address.state).toEqual(expect.any(String));
+      });
+    });
+});

cd279a1335d1446eb2ee21d13fc77a3aaab8d7b1
Gavin Vaske <Gavin.Vaske@gmail.com>
2023-07-23 22:21:32 -0500

    add attributes to Finish db model and tests

--- application/models/finish.js
+++ application/models/finish.js
@@ -2,11 +2,85 @@ const mongoose = require('mongoose');
 mongoose.Schema.Types.String.set('trim', true);
 const Schema = mongoose.Schema;
 
+const NUMBER_OF_PENNIES_IN_A_DOLLAR = 100;
+const NUMBER_OF_DECIMAL_PLACES_IN_CURRENCY = 2;
+
+
+function convertPenniesToDollars(amountInPennies) {
+    return Number((amountInPennies / NUMBER_OF_PENNIES_IN_A_DOLLAR).toFixed(NUMBER_OF_DECIMAL_PLACES_IN_CURRENCY));
+}
+
+function convertDollarsToPennies(dollarAmount) {
+    return parseInt(dollarAmount * NUMBER_OF_PENNIES_IN_A_DOLLAR);
+}
+
 const schema = new Schema({
     name: {
         type: String,
         required: true,
     },
+    finishId: {
+        type: String,
+        required: true
+    },
+    vendor: {
+        type: Schema.Types.ObjectId,
+        ref: 'Vendor',
+        required: true
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'MaterialCategory',
+        required: true
+    },
+    thickness: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    weight: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    finishCost: {
+        type: Number,
+        required: true,
+        min: 0,
+        get: convertPenniesToDollars,
+        set: convertDollarsToPennies,
+    },
+    freightCost: {
+        type: Number,
+        required: true,
+        min: 0,
+        get: convertPenniesToDollars,
+        set: convertDollarsToPennies,
+    },
+    width: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    quotePrice: {
+        type: Number,
+        required: true,
+        min: 0,
+        get: convertPenniesToDollars,
+        set: convertDollarsToPennies,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    whenToUse: {
+        type: String,
+        required: true
+    },
+    alternativeFinish: {
+        type: String,
+        required: false
+    }
 }, { timestamps: true });
 
 const Finish = mongoose.model('Finish', schema);
--- test/models/finish.spec.js
+++ test/models/finish.spec.js
@@ -1,5 +1,7 @@
 const chance = require('chance').Chance();
+const mongoose = require('mongoose');
 const FinishModel = require('../../application/models/finish');
+const databaseService = require('../../application/services/databaseService');
 
 describe('validation', () => {
     let finishAttributes;
@@ -7,7 +9,19 @@ describe('validation', () => {
     beforeEach(() => {
         jest.resetAllMocks();
         finishAttributes = {
-            name: chance.string()
+            name: chance.string(),
+            finishId: chance.string(),
+            vendor: mongoose.Types.ObjectId(),
+            category: mongoose.Types.ObjectId(),
+            thickness: chance.integer({ min: 0 }),
+            weight: chance.d100(),
+            finishCost: chance.floating({ min: 0, fixed: 2 }),
+            freightCost: chance.floating({ min: 0, fixed: 2 }),
+            width: chance.d100(),
+            quotePrice: chance.floating({ min: 0, fixed: 2 }),
+            description: chance.string(),
+            whenToUse: chance.string(),
+            alternativeFinish: chance.string()
         };
     });
 
@@ -20,13 +34,381 @@ describe('validation', () => {
             expect(error).toBe(undefined);
         });
 
-        it('should trim whitespace around "name"', () => {
-            const name = chance.string();
-            finishAttributes.name = ' ' + name + ' ';
+        describe('attribute: name', () => {
+            it('should fail if name is undefined', () => {
+                delete finishAttributes.name;
+                const finish = new FinishModel(finishAttributes);
+                
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
 
-            const finish = new FinishModel(finishAttributes);
+            it('should be a string', () => {
+                finishAttributes.name = chance.string();
+                
+                const finish = new FinishModel(finishAttributes);
+                
+                expect(finish.name).toEqual(expect.any(String));
+            });
 
-            expect(finish.name).toBe(name);
+            it('should trim whitespace around "name"', () => {
+                const name = chance.string();
+                finishAttributes.name = ' ' + name + ' ';
+    
+                const finish = new FinishModel(finishAttributes);
+    
+                expect(finish.name).toBe(name);
+            });
+        });
+
+        describe('attribute: finishId', () => {
+            it('should fail if attribute is undefined', () => {
+                delete finishAttributes.finishId;
+                const finish = new FinishModel(finishAttributes);
+                
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
+
+            it('should be a string', () => {
+                finishAttributes.finishId = chance.string();
+                
+                const finish = new FinishModel(finishAttributes);
+                
+                expect(finish.finishId).toEqual(expect.any(String));
+            });
+        });
+
+        describe('attribute: vendor', () => {
+            it('should fail if attribute is undefined', () => {
+                delete finishAttributes.vendor;
+                const finish = new FinishModel(finishAttributes);
+
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
+
+            it('should fail validation if attribute is not a mongoose ObjectId', () => {
+                finishAttributes.vendor = chance.word();
+                const finish = new FinishModel(finishAttributes);
+                
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
+
+            it('should be a mongoose ObjectId', () => {
+                finishAttributes.vendor = mongoose.Types.ObjectId();
+                
+                const finish = new FinishModel(finishAttributes);
+                
+                expect(finish.vendor).toEqual(expect.any(mongoose.Types.ObjectId));
+            });
+        });
+
+        describe('attribute: category', () => {
+            it('should fail if attribute is undefined', () => {
+                delete finishAttributes.category;
+                const finish = new FinishModel(finishAttributes);
+                
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
+
+            it('should fail validation if attribute is not a mongoose ObjectId', () => {
+                finishAttributes.category = chance.word();
+                const finish = new FinishModel(finishAttributes);
+                
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
+
+            it('should be a mongoose ObjectId', () => {
+                finishAttributes.category = mongoose.Types.ObjectId();
+                
+                const finish = new FinishModel(finishAttributes);
+                
+                expect(finish.category).toEqual(expect.any(mongoose.Types.ObjectId));
+            });
+        });
+
+        describe('attribute: thickness', () => {
+            it('should fail validation if attribute is undefined', () => {
+                delete finishAttributes.thickness;
+                const finish = new FinishModel(finishAttributes);
+                
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
+
+            it('should be a number', () => {
+                finishAttributes.thickness = String(chance.integer({ min: 0 }));
+                
+                const finish = new FinishModel(finishAttributes);
+                
+                expect(finish.thickness).toEqual(expect.any(Number));
+            });
+
+            it('should be greater than or equal to 0', () => {
+                finishAttributes.thickness = -1;
+                const finish = new FinishModel(finishAttributes);
+                
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
+        });
+        describe('attribute: weight', () => {
+            it('should fail validation if attribute is undefined', () => {
+                delete finishAttributes.weight;
+                const finish = new FinishModel(finishAttributes);
+
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
+
+            it('should be a number', () => {
+                finishAttributes.weight = String(chance.integer({ min: 0 }));
+                
+                const finish = new FinishModel(finishAttributes);
+                
+                expect(finish.weight).toEqual(expect.any(Number));
+            });
+
+            it('should be greater than or equal to 0', () => {
+                finishAttributes.weight = -1;
+                const finish = new FinishModel(finishAttributes);
+                
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
+        });
+
+        describe('attribute: finishCost', () => {
+            it('should fail validation if attribute is undefined', () => {
+                delete finishAttributes.finishCost;
+                const finish = new FinishModel(finishAttributes);
+                
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
+
+            it('should be a number', () => {
+                finishAttributes.finishCost = String(chance.floating({ min: 0, fixed: 2 }));
+                
+                const finish = new FinishModel(finishAttributes);
+                
+                expect(finish.finishCost).toEqual(expect.any(Number));
+            });
+
+            it('should be greater than or equal to 0', () => {
+                finishAttributes.finishCost = -1;
+                const finish = new FinishModel(finishAttributes);
+                
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
+
+            it('should only store 2 decimal places', () => {
+                const expectedCost = 123.99;
+                const decimalToIgnore = 0.009;
+                finishAttributes.finishCost = expectedCost + decimalToIgnore;
+
+                const finish = new FinishModel(finishAttributes);
+                
+                expect(finish.finishCost).toBe(expectedCost);
+            });
+        });
+
+        describe('attribute: freightCost', () => {
+            it('should fail validation if attribute is undefined', () => {
+                delete finishAttributes.freightCost;
+                const finish = new FinishModel(finishAttributes);
+                
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
+
+            it('should be a number', () => {
+                finishAttributes.freightCost = String(chance.floating({ min: 0, fixed: 2 }));
+                
+                const finish = new FinishModel(finishAttributes);
+                
+                expect(finish.freightCost).toEqual(expect.any(Number));
+            });
+
+            it('should be greater than or equal to 0', () => {
+                finishAttributes.freightCost = -1;
+                const finish = new FinishModel(finishAttributes);
+
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
+        });
+
+        describe('attribute: width', () => {
+            it('should fail validation if attribute is undefined', () => {
+                delete finishAttributes.width;
+                const finish = new FinishModel(finishAttributes);
+                
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
+
+            it('should be a number', () => {
+                finishAttributes.width = String(chance.integer({ min: 0 }));
+                
+                const finish = new FinishModel(finishAttributes);
+                
+                expect(finish.width).toEqual(expect.any(Number));
+            });
+
+            it('should be greater than or equal to 0', () => {
+                finishAttributes.width = -1;
+                const finish = new FinishModel(finishAttributes);
+                
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
+        });
+
+        describe('attribute: quotePrice', () => {
+            it('should fail validation if attribute is undefined', () => {
+                delete finishAttributes.quotePrice;
+                const finish = new FinishModel(finishAttributes);
+                
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
+
+            it('should be a number', () => {
+                finishAttributes.quotePrice = String(chance.floating({ min: 0, fixed: 2 }));
+                
+                const finish = new FinishModel(finishAttributes);
+                
+                expect(finish.quotePrice).toEqual(expect.any(Number));
+            });
+
+            it('should be greater than or equal to 0.00', () => {
+                finishAttributes.quotePrice = -1;
+                const finish = new FinishModel(finishAttributes);
+                
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
+
+            it('should only store 2 decimal places', () => {
+                const expectedCost = 123.99;
+                const decimalToIgnore = 0.009;
+                finishAttributes.quotePrice = expectedCost + decimalToIgnore;
+                
+                const finish = new FinishModel(finishAttributes);
+                
+                expect(finish.quotePrice).toBe(expectedCost);
+            });
+        });
+
+        describe('attribute: description', () => {
+            it('should fail validation if attribute is undefined', () => {
+                delete finishAttributes.description;
+                const finish = new FinishModel(finishAttributes);
+
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
+
+            it('should be a string', () => {
+                finishAttributes.description = chance.string();
+                
+                const finish = new FinishModel(finishAttributes);
+                
+                expect(finish.description).toEqual(expect.any(String));
+            });
+
+            it('should trim whitespace', () => {
+                const expectedDescription = chance.string();
+                finishAttributes.description = ` ${expectedDescription} `;
+                
+                const finish = new FinishModel(finishAttributes);
+                
+                expect(finish.description).toBe(expectedDescription);
+            });
+        });
+
+        describe('attribute: whenToUse', () => {
+            it('should fail validation if attribute is undefined', () => {
+                delete finishAttributes.whenToUse;
+                const finish = new FinishModel(finishAttributes);
+                
+                const error = finish.validateSync();
+                
+                expect(error).toBeDefined();
+            });
+
+            it('should be a string', () => {
+                finishAttributes.whenToUse = chance.string();
+                
+                const finish = new FinishModel(finishAttributes);
+                
+                expect(finish.whenToUse).toEqual(expect.any(String));
+            });
+
+            it('should trim whitespace', () => {
+                const expectedWhenToUse = chance.string();
+                finishAttributes.whenToUse = ` ${expectedWhenToUse} `;
+                
+                const finish = new FinishModel(finishAttributes);
+                
+                expect(finish.whenToUse).toBe(expectedWhenToUse);
+            });
+        });
+
+        describe('attribute: alternativeFinish', () => {
+            it('should not fail validation if attribute is undefined', () => {
+                delete finishAttributes.alternativeFinish;
+                const finish = new FinishModel(finishAttributes);
+                
+                const error = finish.validateSync();
+                
+                expect(error).toBeUndefined();
+            });
+        });
+    });
+
+    describe('verify timestamps on created object', () => {
+        beforeEach(async () => {
+            await databaseService.connectToTestMongoDatabase();
+        });
+
+        afterEach(async () => {
+            await databaseService.closeDatabase();
+        });
+
+        describe('verify timestamps on created object', () => {
+            it('should have a "createdAt" attribute once object is saved', async () => {
+                const finish = new FinishModel(finishAttributes);
+                let savedFinish = await finish.save({ validateBeforeSave: false });
+
+                expect(savedFinish.createdAt).toBeDefined();
+                expect(savedFinish.updatedAt).toBeDefined();
+            });
         });
     });
 });

0b9fb19fd6293ba0c4cebc38aec9c16ad2760a09
Gavin Vaske <Gavin.Vaske@gmail.com>
2023-07-23 22:21:16 -0500

    remove dev code; test Die timestamps

--- application/models/Die.js
+++ application/models/Die.js
@@ -183,62 +183,6 @@ const schema = new Schema({
         required: false
     },
     // iterations: {},
-
-
-    // dieNumber: {    // all characters (uppercase) - all numbers
-    //     type: String,
-    //     required: true,
-    //     validate: [validateDieNumberFormat, 'The provided dieNumber "{VALUE}" must be in the following format: characters followed a dash followed by numbers'],
-    //     uppercase: true
-    // },
-    // pitch: {
-    //     type: Number,
-    //     required: true,
-    //     set: convertStringToNumberAndRemoveNonDigits
-    // },
-    // gear: {
-    //     type: Number,
-    //     required: true,
-    //     min: 0
-    // },
-    // shape: {
-    //     type: String,
-    //     required: true
-    // },
-    // TODO Future Self: Finsih Post-TDDing the attributes below
-    
-    // sizeAcross: {
-    //     type: Number,
-    //     required: true
-    // },
-    // sizeAround: {
-    //     type: Number,
-    //     required: true
-    // },
-    // matrixAround: {
-    //     type: Number,
-    //     min: 0,
-    //     required: true
-    // },
-    // labelsAcross: {
-    //     type: Number,
-    //     min: 0,
-    //     required: true
-    // },
-    // labelsAround: {
-    //     type: Number,
-    //     min: 0,
-    //     required: true
-    // },
-    // size: {
-    //     type: Number,
-    //     min: 0,
-    //     required: true
-    // },
-    // notes: {
-    //     type: Number,
-    //     required: false
-    // }
 }, { timestamps: true });
 
 const Die = mongoose.model('Die', schema);
--- test/models/die.spec.js
+++ test/models/die.spec.js
@@ -5,6 +5,7 @@ const { toolTypes } = require('../../application/enums/toolTypesEnum');
 const { dieVendors } = require('../../application/enums/dieVendorsEnum');
 const { dieMagCylinders } = require('../../application/enums/dieMagCylindersEnum');
 const { dieStatuses, IN_STOCK_DIE_STATUS, ORDERED_DIE_STATUS } = require('../../application/enums/dieStatusesEnum');
+const databaseService = require('../../application/services/databaseService');
 
 function getRandomCost() {
     return chance.floating({ min: 0, fixed: 2 });
@@ -833,143 +834,25 @@ describe('validation', () => {
         });
     });
     
-    describe('attribute: TODO', () => {});
-    describe('attribute: TODO', () => {});
-    describe('attribute: TODO', () => {});
+    describe('verify timestamps on created object', () => {
+        beforeEach(async () => {
+            await databaseService.connectToTestMongoDatabase();
+        });
 
-    // describe('attribute: dieNumber', () => {
-    //     it('should fail validation if attribute is undefined', () => {
-    //         delete dieAttributes.dieNumber;
-    //         const die = new DieModel(dieAttributes);
+        afterEach(async () => {
+            await databaseService.closeDatabase();
+        });
 
-    //         const error = die.validateSync();
+        describe('verify timestamps on created object', () => {
+            it('should have a "createdAt" attribute once object is saved', async () => {
+                const Die = new DieModel(dieAttributes);
+                let savedDie = await Die.save({ validateBeforeSave: false });
 
-    //         expect(error).toBeDefined();
-    //     });
+                expect(savedDie.createdAt).toBeDefined();
+                expect(savedDie.updatedAt).toBeDefined();
+            });
+        });
+    });
 
-    //     it('should be a string', () => {
-    //         const die = new DieModel(dieAttributes);
 
-    //         expect(die.dieNumber).toEqual(expect.any(String))
-    //     });
-
-    //     it('should fail if the string DOES NOT follow the correct regex pattern', () => {
-    //         const invalidDieNumber = chance.word();
-    //         dieAttributes.dieNumber = invalidDieNumber;
-    //         const die = new DieModel(dieAttributes);
-
-    //         const error = die.validateSync();
-
-    //         expect(error).toBeDefined()
-    //     })
-
-    //     it('should pass validation if the string DOES follow the correct regex pattern', () => {
-    //         const validDieNumber = 'fdkjZKJKfdskljf-4892349823429';
-    //         dieAttributes.dieNumber = validDieNumber;
-    //         const die = new DieModel(dieAttributes);
-
-    //         const error = die.validateSync();
-
-    //         expect(error).not.toBeDefined()
-    //     })
-
-    //     it('should convert the attribute to uppercase', () => {
-    //         const lowerCaseDieNumber = 'abccc-111';
-    //         dieAttributes.dieNumber = lowerCaseDieNumber;
-            
-    //         const die = new DieModel(dieAttributes);
-
-    //         expect(die.dieNumber).toBe(lowerCaseDieNumber.toUpperCase())
-    //     })
-    // });
-
-    // describe('attribute: pitch', () => {
-    //     it('should be a number', () => {
-    //         const die = new DieModel(dieAttributes);
-
-    //         expect(die.pitch).toEqual(expect.any(Number))
-    //     });
-
-    //     it('should fail validation if attribute is not defined', () => {
-    //         delete dieAttributes.pitch;
-    //         const die = new DieModel(dieAttributes);
-
-    //         const error = die.validateSync();
-
-    //         expect(error).toBeDefined();
-    //     })
-
-    //     it('should remove non-digits', () => {
-    //         const expectedPitch = chance.floating({ min: 0 });
-    //         const pitchContainingNonDigits = `fdskfjs*()(*--$#*$${expectedPitch}fjkhfdskjfsfsdf`
-    //         dieAttributes.pitch = pitchContainingNonDigits;
-            
-    //         const die = new DieModel(dieAttributes);
-
-    //         expect(die.pitch).toBe(expectedPitch);
-    //     })
-
-    //     it('should not remove all non-digits except for the decimal points', () => {
-    //         const expectedPitch = 1234567.1234
-    //         const pitchContainingNonDigits = `${expectedPitch} CP`
-    //         dieAttributes.pitch = pitchContainingNonDigits;
-            
-    //         const die = new DieModel(dieAttributes);
-
-    //         expect(die.pitch).toBe(expectedPitch);
-    //     })
-
-    //     it('should handle floating points', () => {
-    //         const expectedPitch = chance.floating({ min: 0 })
-    //         const pitchContainingNonDigits = expectedPitch
-    //         dieAttributes.pitch = pitchContainingNonDigits;
-            
-    //         const die = new DieModel(dieAttributes);
-
-    //         expect(die.pitch).toBe(expectedPitch);
-    //     })
-    // })
-
-    // describe('attribute: gear', () => {
-    //     it('should be a number', () => {
-    //         const die = new DieModel(dieAttributes);
-
-    //         expect(die.gear).toEqual(expect.any(Number))
-    //     });
-
-    //     it('should fail validation if attribute is not defined', () => {
-    //         delete dieAttributes.gear;
-    //         const die = new DieModel(dieAttributes);
-            
-    //         const error = die.validateSync();
-
-    //         expect(error).toBeDefined()
-    //     })
-
-    //     it('should fail validation if attribute is negative', () => {
-    //         dieAttributes.gear = chance.integer({ max: -1 });
-    //         const die = new DieModel(dieAttributes);
-            
-    //         const error = die.validateSync();
-
-    //         expect(error).toBeDefined()
-    //     })
-    // })
-
-    // describe('attribute: shape', () => {
-    //     it('should be a string', () => {
-    //         const die = new DieModel(dieAttributes);
-
-    //         expect(die.shape).toEqual(expect.any(String));
-    //     });
-
-    //     it('should fail validation if attribute is undefined', () => {
-    //         delete dieAttributes.shape;
-    //         const die = new DieModel(dieAttributes);
-
-    //         const error = die.validateSync()
-
-    //         expect(error).toBeDefined();
-    //     })
-    // })
 });

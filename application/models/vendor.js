e182c93485c6a2fe1456418d29852d47164d4f84
Gavin Vaske <Gavin.Vaske@gmail.com>
2023-07-24 22:11:32 -0500

    wip: begin populating attributes to Vendor database schema

--- application/models/vendor.js
+++ application/models/vendor.js
@@ -1,16 +1,37 @@
 const mongoose = require('mongoose');
 mongoose.Schema.Types.String.set('trim', true);
 const Schema = mongoose.Schema;
-const { validatePhoneNumber } = require('../services/dataValidationService');
+const { validatePhoneNumber, validateEmail } = require('../services/dataValidationService');
 
 const schema = new Schema({
     name: {
         type: String,
         required: true
     },
-    phone: {
+    phoneNumber: {
         type: String,
         validate: [validatePhoneNumber, 'The provided phone number "{VALUE}" is not a valid phone number']
+    },
+    email: {
+        type: String,
+        required: false,
+        validate: [validateEmail, 'The provided email "{VALUE}" is not a valid email address']
+    },
+    notes: {
+        type: String,
+        required: false
+    },
+    website: {
+        type: String,
+        required: false
+    },
+    // address: {   // TOOD: Add address attribute and validate using google maps API
+    //     type: String,
+    //     required: true
+    // },
+    primaryContactName: {
+        type: String,
+        required: true
     }
 }, { timestamps: true });
 
--- test/models/vendor.spec.js
+++ test/models/vendor.spec.js
@@ -7,17 +7,39 @@ describe('validation', () => {
     beforeEach(() => {
         jest.resetAllMocks();
         vendorAttributes = {
-            name: chance.string()
+            name: chance.string(),
+            phoneNumber: chance.phone(),
+            email: chance.email(),
+            notes: chance.paragraph(),
+            website: chance.url(),
+            primaryContactName: chance.string(),
         };
     });
 
-    describe('successful validation', () => {
-        it('should validate when required attributes are defined', () => {
+    it('should validate when required attributes are defined', () => {
+        const vendor = new VendorModel(vendorAttributes);
+
+        const error = vendor.validateSync();
+
+        expect(error).toBe(undefined);
+    });
+
+    describe('attribute: name', () => {
+        it('should be required', () => {
+            delete vendorAttributes.name;
             const vendor = new VendorModel(vendorAttributes);
-    
+            
             const error = vendor.validateSync();
-    
-            expect(error).toBe(undefined);
+            
+            expect(error).toBeDefined();
+        });
+
+        it('should be a string', () => {
+            vendorAttributes.name = chance.string();
+            
+            const vendor = new VendorModel(vendorAttributes);
+        
+            expect(vendor.name).toEqual(expect.any(String));
         });
 
         it('should trim whitespace around "name"', () => {
@@ -27,6 +49,141 @@ describe('validation', () => {
             const vendor = new VendorModel(vendorAttributes);
 
             expect(vendor.name).toBe(name);
+        });
+    });
+
+    describe('attribute: phoneNumber', () => {
+        it('should NOT be required', () => {
+            delete vendorAttributes.phoneNumber;
+            const vendor = new VendorModel(vendorAttributes);
+            
+            const error = vendor.validateSync();
+            
+            expect(error).toBeUndefined();
+        });
+
+        it('should pass validation if phoneNumber is valid', () => {
+            vendorAttributes.phoneNumber = chance.phone();
+            const vendor = new VendorModel(vendorAttributes);
+            
+            const error = vendor.validateSync();
+            
+            expect(error).toBeUndefined();
+        });
+
+        it('should fail validation if phoneNumber is not valid', () => {
+            vendorAttributes.phoneNumber = chance.string();
+            const vendor = new VendorModel(vendorAttributes);
+            
+            const error = vendor.validateSync();
+            
+            expect(error).toBeDefined();
+        });
+
+        it('should not fail validation if phoneNumber is empty', () => {
+            vendorAttributes.phoneNumber = '';
+            const vendor = new VendorModel(vendorAttributes);
+            
+            const error = vendor.validateSync();
+            
+            expect(error).toBeUndefined();
+        });
+    });
+
+    describe('attribute: email', () => {
+        it('should NOT be required', () => {
+            delete vendorAttributes.email;
+            const vendor = new VendorModel(vendorAttributes);
+
+            const error = vendor.validateSync();
+            
+            expect(error).toBeUndefined();
+        });
+
+        it('should pass validation if email is valid', () => {
+            vendorAttributes.email = chance.email();
+            const vendor = new VendorModel(vendorAttributes);
+            
+            const error = vendor.validateSync();
+
+            expect(error).toBeUndefined();
+        });
+
+        it('should fail validation if email is not valid', () => {
+            vendorAttributes.email = chance.word();
+            const vendor = new VendorModel(vendorAttributes);
+            
+            const error = vendor.validateSync();
+            
+            expect(error).toBeDefined();
+        });
+
+        it('should not fail validation if email is empty', () => {
+            vendorAttributes.email = '';
+            const vendor = new VendorModel(vendorAttributes);
+            
+            const error = vendor.validateSync();
+            
+            expect(error).toBeUndefined();
+        });
+    });
+
+    describe('attribute: notes', () => {
+        it('should NOT be required', () => {
+            delete vendorAttributes.notes;
+            const vendor = new VendorModel(vendorAttributes);
+            
+            const error = vendor.validateSync();
+            
+            expect(error).toBeUndefined();
+        });
+
+        it('should trim whitespace around "notes"', () => {
+            const notes = chance.string();
+            vendorAttributes.notes = ' ' + notes + ' ';
+            
+            const vendor = new VendorModel(vendorAttributes);
+            
+            expect(vendor.notes).toBe(notes);
+        });
+    });
+
+    describe('attribute: website', () => {
+        it('should NOT be required', () => {
+            delete vendorAttributes.website;
+            const vendor = new VendorModel(vendorAttributes);
+            
+            const error = vendor.validateSync();
+            
+            expect(error).toBeUndefined();
+        });
+
+        it('should trim whitespace around "website"', () => {
+            const website = chance.string();
+            vendorAttributes.website ='  ' + website + ' ';
+            
+            const vendor = new VendorModel(vendorAttributes);
+            
+            expect(vendor.website).toBe(website);
+        });
+    });
+
+    describe('attribute: primaryContactName', () => {
+        it('should be required', () => {
+            delete vendorAttributes.primaryContactName;
+            const vendor = new VendorModel(vendorAttributes);
+            
+            const error = vendor.validateSync();
+            
+            expect(error).toBeDefined();
+        });
+
+        it('should be a string', () => {
+            vendorAttributes.primaryContactName = chance.string();
+            
+            const vendor = new VendorModel(vendorAttributes);
+            
+            expect(vendor.primaryContactName).toEqual(expect.any(String));
         });
     });
 });

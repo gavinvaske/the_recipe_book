a382720da9dc2009c05d750276bff4e254e0f53d
Gavin Vaske <Gavin.Vaske@gmail.com>
2023-11-03 19:20:34 -0500

    add baseProductService.getCombinedMaterialThicknessByBaseProductId(...)

---
+++ application/services/baseProductService.js
@@ -0,0 +1,18 @@
+const BaseProductModel = require('../../application/models/baseProduct');
+
+module.exports.getCombinedMaterialThicknessByBaseProductId = async (baseProductId) => {
+    const baseProduct = await BaseProductModel
+        .findById(baseProductId)
+        .populate({path: 'primaryMaterial'})
+        .populate({path: 'secondaryMaterial'})
+        .populate({path: 'finish'})
+        .exec();
+
+    const { primaryMaterial, secondaryMaterial, finish } = baseProduct;
+    const primaryMaterialThickness = (primaryMaterial && primaryMaterial.thickness) ? primaryMaterial.thickness : 0;
+    const secondaryMaterialThickness = (secondaryMaterial && secondaryMaterial.thickness) ? secondaryMaterial.thickness : 0;
+    const finishThickness = (finish && finish.thickness) ? finish.thickness : 0;
+    const combinedThickness = primaryMaterialThickness + secondaryMaterialThickness + finishThickness
+
+    return combinedThickness;
+}

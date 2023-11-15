db2e1af963d46919212f2e9e7d5597bb702afa8e
Gavin Vaske <Gavin.Vaske@gmail.com>
2023-11-14 18:32:24 -0600

    fix occasional floating point issue in calculation using Decimal.js library

--- application/services/packagingService.js
+++ application/services/packagingService.js
@@ -1,5 +1,6 @@
 const { howManyCirclesCanFitInThisSquare } = require('../enums/circlesPerSquareEnum');
 const isNil = require('lodash.isnil');
+const Decimal = require('decimal.js');
 
 const ONE_EIGHTH_INCH = 0.125;
 
@@ -8,9 +9,9 @@ module.exports.getNumberOfLayers = (boxHeight, rollHeight) => {
         return 0;
     }
     const buffer = ONE_EIGHTH_INCH;
-    const boxHeightMinusBuffer = boxHeight - buffer;
+    const boxHeightMinusBuffer = new Decimal(boxHeight - buffer);
 
-    return Math.floor(boxHeightMinusBuffer / rollHeight);
+    return Math.floor(boxHeightMinusBuffer.dividedBy(rollHeight));
 };
 
 module.exports.getRollsPerLayer = (rollDiameter, boxSideLength) => {
@@ -25,6 +26,7 @@ module.exports.getRollsPerLayer = (rollDiameter, boxSideLength) => {
 
 module.exports.getNumberOfBoxes = (rollsPerBox, numberOfRolls) => {
     if (isNil(rollsPerBox) || isNil(numberOfRolls)) return null;
+    const numberOfRollsWithMorePrecision = new Decimal(numberOfRolls);
 
-    return Math.ceil(numberOfRolls / rollsPerBox);
+    return Math.ceil(numberOfRollsWithMorePrecision.dividedBy(rollsPerBox));
 };

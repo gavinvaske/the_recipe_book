47a21bc70dea281c657d4391924187a25a3db0b3
Gavin Vaske <Gavin.Vaske@gmail.com>
2023-11-17 15:30:13 -0600

    fix lint errors

--- test/services/packagingService.spec.js
+++ test/services/packagingService.spec.js
@@ -17,16 +17,15 @@ describe('File: packagingService.js', () => {
     });
 
     describe('Function: getNumberOfLayers()', () => {
-        let boxHeight, rollHeight;
+        const rollHeight = 12.33;
+        let boxHeight;
 
         beforeEach(() => {
-            rollHeight = chance.d100();
             boxHeight = chance.floating({ min: 0, max: 100}) + chance.floating({ min: 0, max: 100});
         });
 
         it('should return 0 if no layer can fit with more than a 1/8 inch buffer gap from the layer to the top of the box', () => {
             const slightlyLessThanOneEighthInch = (ONE_EIGHTH_INCH_BUFFER - 0.001); // eslint-disable-line no-magic-numbers
-            rollHeight = chance.d100();
             boxHeight = rollHeight + slightlyLessThanOneEighthInch;
             const expectedNumberOfLayers = 0;
 
@@ -36,7 +35,6 @@ describe('File: packagingService.js', () => {
         });
 
         it('should allow layer if box has a 1/8 inch buffer', () => {
-            rollHeight = chance.d100();
             boxHeight = rollHeight + ONE_EIGHTH_INCH_BUFFER;
             const expectedNumberOfLayers = 1;
 
@@ -47,7 +45,6 @@ describe('File: packagingService.js', () => {
 
         it('should only compute integer multiples of layers', () => {
             const expectedNumberOfLayers = chance.d100();
-            rollHeight = 12.333;
             boxHeight = (rollHeight * expectedNumberOfLayers) + ONE_EIGHTH_INCH_BUFFER;
 
             const actualNumberOfLayers = packagingService.getNumberOfLayers(boxHeight, rollHeight);

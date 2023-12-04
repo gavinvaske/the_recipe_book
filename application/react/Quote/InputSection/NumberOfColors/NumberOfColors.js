3625337a3f4c8485d48b7b0b55ac93e758589dfc
Gavin Vaske <Gavin.Vaske@gmail.com>
2023-12-03 22:13:39 -0600

    wip: work on NumberOfColors react component

--- application/react/Quote/InputSection/NumberOfColors/NumberOfColors.js
+++ application/react/Quote/InputSection/NumberOfColors/NumberOfColors.js
@@ -3,8 +3,27 @@ import './NumberOfColors.scss';
 
 export default NumberOfColors = () => {
   return (
-    <div className="number-of-colors-section">
-      TODO: Number of Colors
+  <div className="number-of-colors-section">
+    <div>
+      <div>Number of Colors:</div>
+      <div className='colors-section'>
+        <div className='row'>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+        </div>
+        <div className='row'>
+          <div>5</div>
+          <div>6</div>
+          <div>7</div>
+        </div>
+      </div>
     </div>
+    <div>
+      <h1>5</h1>
+      <h3>CMYK</h3>
+    </div>
+  </div>
   );
 }

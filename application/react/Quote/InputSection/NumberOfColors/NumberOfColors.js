721751f887900234cc49c75dbd614522b3196539
Gavin Vaske <Gavin.Vaske@gmail.com>
2023-12-06 18:20:40 -0600

    implement NumberOfColors component

--- application/react/Quote/InputSection/NumberOfColors/NumberOfColors.js
+++ application/react/Quote/InputSection/NumberOfColors/NumberOfColors.js
@@ -1,30 +1,59 @@
-import React from 'react';
+import React, { useState} from 'react';
 import './NumberOfColors.scss';
 import quoteStore from '../../../stores/quoteStore'
 
 export default NumberOfColors = () => {
+  const DEFAULT_COLOR_TO_COUNT = {
+    'C': 0,
+    'M': 0,
+    'Y': 0,
+    'K': 0,
+    'W': 0,
+    'O': 0,
+    'V': 0,
+  }
+  const { quoteInputs } = quoteStore;
+  const [colorToCount, setColorToCount] = useState(DEFAULT_COLOR_TO_COUNT)
+
+  const updateColorCount = (color) => {
+    console.log('updating color! -> ', color)
+    colorToCount[color] = colorToCount[color] + 1;
+
+    setColorToCount({ ...colorToCount })
+    const colorCount = getNumberOfColors();
+    quoteInputs.numberOfColorsOverride = colorCount;
+  }
+
+  const getNumberOfColors = () => {
+    return Object.values(colorToCount).reduce((sum, current) => {
+      return sum + current
+    }, 0)
+  }
+
+  const getUniqueColors = () => {
+    return Object.keys(colorToCount).filter((color) => colorToCount[color] > 0).join(' ')
+  }
+
+  const resetNumberOfColors = () => {
+    setColorToCount({...DEFAULT_COLOR_TO_COUNT});
+    quoteInputs.numberOfColorsOverride = undefined;
+  }
+
   return (
-  <div className="number-of-colors-section">
-    <div>
-      <div>Number of Colors:</div>
-      <div className='colors-section'>
-        <div className='row'>
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
+    <>
+      <div className='number-of-colors-section'>
+        <div className='colors-input-section'>
+          {Object.keys(colorToCount).map((color) => (
+            <div onClick={(e) => updateColorCount(color)}>{color} : {colorToCount[color]}</div>)
+          )}
         </div>
-        <div className='row'>
-          <div>5</div>
-          <div>6</div>
-          <div>7</div>
+        <div className='colors-output-section'>
+        <h1> Sum = {quoteInputs.numberOfColorsOverride} </h1>
+        <h3> Unique Colors: {getUniqueColors()} </h3>
         </div>
       </div>
-    </div>
-    <div>
-      <TextField accessor={'numberOfcolorsOverride'} header={'Number of Colors'} onChange={(e) => quoteStore.quoteInputs.numberOfColorsOverride = e.target.value}></TextField>
-      <h3>CMYK</h3>
-    </div>
-  </div>
+
+      <div id='reset-colors' onClick={(e) => resetNumberOfColors()}>Reset NumberOfColors</div>
+    </>
   );
 }

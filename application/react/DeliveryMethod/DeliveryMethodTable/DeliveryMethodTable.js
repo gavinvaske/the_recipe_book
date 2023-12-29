b5ee4763603f7d46006af04e96e68231d82c863a
Gavin Vaske <Gavin.Vaske@gmail.com>
2023-12-28 21:56:53 -0600

    feature: add a basic working table using @tanstack/react-table

--- application/react/DeliveryMethod/DeliveryMethodTable/DeliveryMethodTable.js
+++ application/react/DeliveryMethod/DeliveryMethodTable/DeliveryMethodTable.js
@@ -1,37 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './DeliveryMethodTable.scss';
+import * as React from 'react'
+import axios from 'axios'
+import './DeliveryMethodTable.scss'
 
-const DeliveryMethodTable = () => {
-  const [deliveryMethods, setDeliveryMethods] = useState([]);
-  
-  useEffect(() => {
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
+
+const columnHelper = createColumnHelper()
+
+const columns = [
+  columnHelper.accessor('name', {
+    header: 'Name'
+  }),
+]
+
+function DeliveryMethodTable() {
+  const [deliveryMethods, setDeliveryMethods] = React.useState([])
+
+  React.useEffect(() => {
     axios.get('/delivery-methods?responseDataType=JSON')
-     .then((response) => {
-         const { data } = response;
-         setDeliveryMethods(data);
-      })
-     .catch((error) => {
-        alert('Error loading Delivery Methods: ' +  JSON.stringify(error));
-      })
+    .then((response) => {
+        const { data } = response;
+        setDeliveryMethods(data);
+     })
+    .catch((error) => {
+       alert('Error loading Delivery Methods: ' +  JSON.stringify(error));
+     })
   }, [])
 
+  const table = useReactTable({
+    data: deliveryMethods,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
   return (
-    <div id='delivery-method-table'>
-      TODO: DeliveryMethodTable.js
-      <br></br>
-      <br></br>
-      {
-        deliveryMethods.map((deliveryMethod) => {
-          return (
-            <div key={deliveryMethod._id}>
-              Name: {deliveryMethod.name}
+    <div className="p-2">
+      <div className='table'>
+        <div className='table-header'>
+          {table.getHeaderGroups().map(headerGroup => (
+            <div className='row' key={headerGroup.id}>
+              {headerGroup.headers.map(header => (
+                <div className='row-header' key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </div>
+              ))}
             </div>
-          )
-        })
-      }
+          ))}
+        </div>
+        <div className='table-body'>
+          {table.getRowModel().rows.map(row => (
+            <div className='row' key={row.id}>
+              {row.getVisibleCells().map(cell => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="h-4" />
     </div>
   )
-};
+}
 
 export default DeliveryMethodTable;

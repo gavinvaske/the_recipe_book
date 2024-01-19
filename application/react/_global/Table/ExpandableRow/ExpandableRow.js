940c7109d152d193134d307a1b2db57f80dabea7
Gavin Vaske <Gavin.Vaske@gmail.com>
2024-01-18 19:55:59 -0600

    dont make cursor a pointer on non-expandable rows

--- application/react/_global/Table/ExpandableRow/ExpandableRow.js
+++ application/react/_global/Table/ExpandableRow/ExpandableRow.js
@@ -9,7 +9,7 @@ const ExpandableRow = (props) => {
   const toggleExpanded = () => setExpanded(!expanded);
 
   return (
-    <div className='expandable-row' onClick={() => toggleExpanded()} key={row.id}>
+    <div className='expandable-row' onClick={() => toggleExpanded()} key={row.id} style={{cursor: children ? 'pointer' : ''}}>
       <Row row={row} />
       { expanded && children }
     </div>

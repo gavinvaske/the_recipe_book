b65a7359ca40923b8f6468d334a51de4c0ac3dbb
Gavin Vaske <Gavin.Vaske@gmail.com>
2024-05-25 15:33:14 -0600

    switch endpoint from /material-inventory -> /materials/inventory

--- application/controllers/materialController.js
+++ application/controllers/materialController.js
@@ -4,15 +4,21 @@ const { verifyJwtToken } = require('../middleware/authorize');
 const VendorModel = require('../models/vendor');
 const MaterialCategoryModel = require('../models/materialCategory');
 
+const materialInventoryService = require('../services/materialInventoryService');
+const materialService = require('../services/materialService');
+const purchaseOrderService = require('../services/purchaseOrderService');
+const ticketService = require('../services/ticketService');
+const mongooseService = require('../services/mongooseService');
+
 const SHOW_ALL_MATERIALS_ENDPOINT = '/materials';
 
 router.use(verifyJwtToken);
 
 router.get('/all', async (request, response) => {
     try {
-        
+
         const materials = await MaterialModel.find().exec();
-        
+
         return response.send(materials);
 
     } catch (error) {
@@ -24,7 +30,7 @@ router.get('/all', async (request, response) => {
 router.get('/', async (request, response) => {
     try {
         const materials = await MaterialModel.find().exec();
-        
+
         return response.render('viewMaterials', {
             materials: materials
         });
@@ -62,9 +68,9 @@ router.get('/update/:id', async (request, response) => {
         const materialCategories = await MaterialCategoryModel.find().exec();
 
         return response.render('updateMaterial', {
-            material, 
+            material,
             vendors,
-            materialCategories 
+            materialCategories
         });
     } catch (error) {
         console.log(error);
@@ -99,6 +105,49 @@ router.get('/delete/:id', async (request, response) => {
     }
 
     return response.redirect('back');
+});
+
+router.get('/inventory', async (request, response) => {
+    console.log('hiiii');
+    try {
+        const allMaterials = await materialService.getAllMaterials();
+
+        const distinctMaterialObjectIds = mongooseService.getObjectIds(allMaterials);
+        const distinctMaterialIds = materialService.getMaterialIds(allMaterials);
+
+        const allPurchaseOrders = await purchaseOrderService.getPurchaseOrdersForMaterials(distinctMaterialObjectIds);
+        const materialIdToPurchaseOrders = materialInventoryService.mapMaterialIdToPurchaseOrders(distinctMaterialObjectIds, allPurchaseOrders);
+
+        const purchaseOrdersThatHaveArrived = purchaseOrderService.findPurchaseOrdersThatHaveArrived(allPurchaseOrders);
+        const purchaseOrdersThatHaveNotArrived = purchaseOrderService.findPurchaseOrdersThatHaveNotArrived(allPurchaseOrders);
+
+        const lengthOfAllMaterialsInInventory = purchaseOrderService.computeLengthOfMaterial(purchaseOrdersThatHaveArrived);
+        const lengthOfAllMaterialsOrdered = purchaseOrderService.computeLengthOfMaterial(purchaseOrdersThatHaveNotArrived);
+
+        const materialObjectIdToLengthUsedByTickets = await ticketService.getLengthOfEachMaterialUsedByTickets(distinctMaterialIds);
+
+        const materialInventories = allMaterials.map((material) => {
+            const feetOfMaterialAlreadyUsedByTickets = materialObjectIdToLengthUsedByTickets[material.materialId] || 0;
+            const purchaseOrdersForMaterial = materialIdToPurchaseOrders[material._id];
+
+            return materialInventoryService.buildMaterialInventory(material, purchaseOrdersForMaterial, feetOfMaterialAlreadyUsedByTickets);
+        });
+
+        const netLengthOfMaterialInInventory = materialInventoryService.computeNetLengthOfMaterialInInventory(materialInventories);
+
+        return response.send({
+            materialInventories,
+            lengthOfAllMaterialsInInventory,
+            lengthOfAllMaterialsOrdered,
+            netLengthOfMaterialInInventory
+        });
+
+    } catch (error) {
+        console.log(`An error occurred while attempting to load /material/inventory: ${error}`);
+
+        request.flash('errors', [`The following error occurred: ${error}`]);
+        return response.redirect('back');
+    }
 });
 
 module.exports = router;
--- application/index.js
+++ application/index.js
@@ -68,7 +68,6 @@ app.use('/vendors', require('./controllers/vendorController'));
 app.use('/material-orders', require('./controllers/materialOrdersController'));
 app.use('/tickets', require('./controllers/ticketController'));
 app.use('/products', require('./controllers/productController'));
-app.use('/material-inventory', require('./controllers/materialInventoryController'));
 app.use('/hold-reasons', require('./controllers/holdReasonController'));
 app.use('/proofs', require('./controllers/proofController'));
 app.use('/die-lines', require('./controllers/dieLineController'));

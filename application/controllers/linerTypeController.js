3b3e650b7675adcd114da04de79f763a0a225f56
Gavin Vaske <Gavin.Vaske@gmail.com>
2024-06-10 21:02:36 -0600

    fix lint errors

--- application/controllers/linerTypeController.js
+++ application/controllers/linerTypeController.js
@@ -7,14 +7,14 @@ router.use(verifyJwtToken);
 const HTTP_SERVER_ERROR = 500;
 
 router.get('/', async (_, response) => {
-  try {
-    const linerTypes = await LinerTypeModel.find().exec();
+    try {
+        const linerTypes = await LinerTypeModel.find().exec();
     
-    return response.send(linerTypes);
-  } catch (error) {
-    console.log('Error fetching liner types: ', error);
-    return response.status(HTTP_SERVER_ERROR).send(error.message)
-  }
+        return response.send(linerTypes);
+    } catch (error) {
+        console.log('Error fetching liner types: ', error);
+        return response.status(HTTP_SERVER_ERROR).send(error.message);
+    }
 });
 
 router.post('/', async (request, response) => {

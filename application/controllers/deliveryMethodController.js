8c270f93191e898ebd75a421d18959973714b493
Gavin Vaske <Gavin.Vaske@gmail.com>
2024-06-10 21:13:06 -0600

    fix lint errors

--- application/controllers/deliveryMethodController.js
+++ application/controllers/deliveryMethodController.js
@@ -9,13 +9,13 @@ const BAD_REQUEST_STATUS_CODE = 400;
 const SERVER_ERROR_STATUS_CODE = 500;
 
 router.get('/', async (request, response) => {
-  try {
-    const deliveryMethods = await DeliveryMethodModel.find().exec();
+    try {
+        const deliveryMethods = await DeliveryMethodModel.find().exec();
 
-    return response.send(deliveryMethods);
-  } catch (error) {
-    return response.status(SERVER_ERROR_STATUS_CODE).send(error.message)
-  }
+        return response.send(deliveryMethods);
+    } catch (error) {
+        return response.status(SERVER_ERROR_STATUS_CODE).send(error.message);
+    }
 });
 
 router.post('/', async (request, response) => {

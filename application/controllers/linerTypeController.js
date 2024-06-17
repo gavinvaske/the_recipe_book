551d190e6001c1a4edcc14bed439002cce39408f
Gavin Vaske <Gavin.Vaske@gmail.com>
2024-06-16 18:04:51 -0600

    fix lint errors

--- application/controllers/linerTypeController.js
+++ application/controllers/linerTypeController.js
@@ -8,13 +8,13 @@ const HTTP_SERVER_ERROR = 500;
 const SUCCESS_HTTP_STATUS = 200;
 
 router.delete('/:mongooseId', async (request, response) => {
-  try {
-    await LinerTypeModel.findByIdAndDelete(request.params.mongooseId).exec();
+    try {
+        await LinerTypeModel.findByIdAndDelete(request.params.mongooseId).exec();
 
-    return response.status(SUCCESS_HTTP_STATUS);
-} catch (error) {
-    return response.status(HTTP_SERVER_ERROR).send(error.message);
-}
+        return response.status(SUCCESS_HTTP_STATUS);
+    } catch (error) {
+        return response.status(HTTP_SERVER_ERROR).send(error.message);
+    }
 });
 
 router.get('/', async (_, response) => {
@@ -29,32 +29,32 @@ router.get('/', async (_, response) => {
 });
 
 router.get('/:mongooseId', async (request, response) => {
-  try {
-    const linerType = await LinerTypeModel.findById(request.params.mongooseId);
+    try {
+        const linerType = await LinerTypeModel.findById(request.params.mongooseId);
 
-    return response.json(linerType)
-  } catch (error) {
-    console.log('Error searching for linerType: ', error.message);
-    return response.status(HTTP_SERVER_ERROR).send(error.message);
-  }
+        return response.json(linerType);
+    } catch (error) {
+        console.log('Error searching for linerType: ', error.message);
+        return response.status(HTTP_SERVER_ERROR).send(error.message);
+    }
 });
 
 router.patch('/:mongooseId', async (request, response) => {
-  try {
-    const updatedLinerType = await LinerTypeModel.findOneAndUpdate(
-      {_id: request.params.mongooseId}, 
-      {$set: request.body}, 
-      {runValidators: true, new: true}
-    ).exec();
+    try {
+        const updatedLinerType = await LinerTypeModel.findOneAndUpdate(
+            {_id: request.params.mongooseId}, 
+            {$set: request.body}, 
+            {runValidators: true, new: true}
+        ).exec();
 
-    return response.json(updatedLinerType)
-  } catch(error) {
-    console.log('Failed to update linerType: ', error.message, '\nThe attributes used were: ', JSON.stringify(request.body));
+        return response.json(updatedLinerType);
+    } catch (error) {
+        console.log('Failed to update linerType: ', error.message, '\nThe attributes used were: ', JSON.stringify(request.body));
 
-    response.status(HTTP_SERVER_ERROR).send(error.message)
-  }
+        response.status(HTTP_SERVER_ERROR).send(error.message);
+    }
 
-})
+});
 
 router.post('/', async (request, response) => {
     try {

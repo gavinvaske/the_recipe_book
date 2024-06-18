4aafa98c8bad1b26c1788119564cc2b212772db5
Gavin Vaske <Gavin.Vaske@gmail.com>
2024-06-17 22:48:20 -0600

    setup MaterialForm input fields

--- application/react/Material/MaterialForm/MaterialForm.tsx
+++ application/react/Material/MaterialForm/MaterialForm.tsx
@@ -8,16 +8,10 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import flashMessageStore from '../../stores/flashMessageStore';
 
-const foobarOptions: SelectOption[] = [
-  {
-    displayName: 'foo-1',
-    value: 'bar'
-  },
-  {
-    displayName: 'foo-2',
-    value: 'bart'
-  }
-]
+const vendors: SelectOption[] = []
+const materialCategories: SelectOption[] = []
+const adhesiveCategories: SelectOption[] = []
+const linerTypes: SelectOption[] = []
 
 export const MaterialForm = () => {
   const { register, handleSubmit, formState: { errors } } = useForm<MaterialFormAttributes>();
@@ -43,10 +37,174 @@ export const MaterialForm = () => {
         isRequired={true}
         errors={errors}
       />
+      <Input
+        attribute='materialId'
+        label="Material ID"
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Input
+        attribute='thickness'
+        label="Thickness"
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Input
+        attribute='weight'
+        label="Weight"
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Input
+        attribute='costPerMsi'
+        label="Cost (per MSI)"
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Input
+        attribute='freightCostPerMsi'
+        label="Freight Cost (per MSI)"
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Input
+        attribute='width'
+        label="Width"
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Input
+        attribute='faceColor'
+        label="Face Color"
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Input
+        attribute='adhesive'
+        label="Adhesive"
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Input
+        attribute='quotePricePerMsi'
+        label="Quote Price (Per MSI)"
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Input
+        attribute='description'
+        label="Description"
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Input
+        attribute='whenToUse'
+        label="When-to-use"
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Input
+        attribute='alternativeStock'
+        label="Alternative Stock"
+        register={register}
+        isRequired={false}
+        errors={errors}
+      />
+      <Input
+        attribute='length'
+        label="Length"
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Input
+        attribute='facesheetWeightPerMsi'
+        label="Facesheet Weight (Per MSI)"
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Input
+        attribute='adhesiveWeightPerMsi'
+        label="Adhesive Weight (Per MSI)"
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Input
+        attribute='linerWeightPerMsi'
+        label="Liner Weight (Per MSI)"
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Input
+        attribute='location'
+        label="Location"
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Input
+        attribute='productNumber'
+        label="Product Number"
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Input
+        attribute='masterRollSize'
+        label="Master Roll Size"
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Input
+        attribute='image'
+        label="Image"
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Select
+        attribute='linerType'
+        label="Liner Type"
+        options={linerTypes}
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Select
+        attribute='adhesiveCategory'
+        label="Adhesive Category"
+        options={adhesiveCategories}
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
       <Select
         attribute='vendor'
         label="Vendor"
-        options={foobarOptions}
+        options={vendors}
+        register={register}
+        isRequired={true}
+        errors={errors}
+      />
+      <Select
+        attribute='materialCategory'
+        label="Material Category"
+        options={materialCategories}
         register={register}
         isRequired={true}
         errors={errors}

ead1ed2f7b8298da6415915f636841694922634c
OldRustyDayton <Russidayton73@gmail.com>
2024-06-21 09:28:49 -0500

    material modal styles partially done.

--- application/react/_global/FullscreenModal/FullscreenModal.tsx
+++ application/react/_global/FullscreenModal/FullscreenModal.tsx
@@ -10,8 +10,19 @@ export const FullscreenModal = (props: Props) => {
   const { onClose, children } = props;
   return (
     <div className='modal material-inventory-modal'>
-      <div onClick={() => onClose()}>Click Here to Close</div>
-      {children}
+      <div className='modal-box'>
+        <div className='modal-header'>
+          <i className='fa-light fa-xmark close-modal' onClick={() => onClose()}></i>
+        </div>
+        <div className='modal-body'>
+          <div className='left-body'>
+
+          </div>
+          <div className='right-body'>
+            {children}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

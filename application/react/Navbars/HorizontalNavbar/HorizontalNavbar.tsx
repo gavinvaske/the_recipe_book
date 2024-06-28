dd6a8ae02b89dccf5288bc90fbe68076a9679083
Gavin Vaske <Gavin.Vaske@gmail.com>
2024-06-27 19:14:28 -0600

    setup Layout component and create placeholder Navbar and Footer

---
+++ application/react/Navbars/HorizontalNavbar/HorizontalNavbar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './HorizontalNavbar.scss';
+
+export const HorizontalNavbar = () => {
+  return (
+    <div>TODO @STORM: Build Navbar Here</div>
+  )
+};
---
+++ application/react/_layouts/TopNavbarLayout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { HorizontalNavbar } from '../Navbars/HorizontalNavbar/HorizontalNavbar';
+import { Footer } from '../Footer/Footer';
+
+export const TopNavbarLayout = () => {
+  return (
+    <>
+      <HorizontalNavbar />
+      <Outlet />
+      <Footer />
+    </>
+  )
+};

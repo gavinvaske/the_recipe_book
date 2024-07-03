e806caa052fc5d5db2dcbb8d5bb160f0990dade8
Gavin Vaske <Gavin.Vaske@gmail.com>
2024-07-02 18:39:27 -0600

    setup new DB table: MaterialInventoryEntry

---
+++ application/models/materialInventoryEntry.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+mongoose.Schema.Types.String.set('trim', true);
+const Schema = mongoose.Schema;
+const mongoose_delete = require('mongoose-delete');
+
+/* 
+  * This table is responsible for Adding or Subtracting material from Inventory.
+  * This primary will be used by an Admin to reconcile sporatic changes to the inventory.
+  * Other methods for adding or subtracting materials are the "Ticket" and "MaterialOrder" db tables.
+*/
+const schema = new Schema({
+    material: {
+        type: Schema.Types.ObjectId,
+        ref: 'Material',
+        required: true
+    },
+    length: {
+        type: Number,
+        required: true
+    },
+    notes: {
+        type: String,
+        required: false
+    }
+}, { timestamps: true, strict: 'throw' });
+
+schema.plugin(mongoose_delete, {overrideMethods: true});
+
+const MaterialCategory = mongoose.model('MaterialInventoryEntry', schema);
+
+module.exports = MaterialCategory;

7d8804a9376cacc11defb596935c6a7488acb5fe
Gavin Vaske <Gavin.Vaske@gmail.com>
2024-07-21 12:03:24 -0600

    fix failing test, add new test for react helper method

--- application/react/_helperFunctions/dateTime.spec.ts
+++ application/react/_helperFunctions/dateTime.spec.ts
@@ -13,12 +13,21 @@ describe('File: dateTime', () => {
       expect(result).toBe('N/A')
     })
 
-    it.only('should return the correctly formatted date', () => {
+    it('should return the correctly formatted date', () => {
       const date = new Date('January 01 2020')
 
       const result = dateTime.getDayMonthYear(date)
 
-      expect(result).toBe('January 1st, 2020')
+      expect(result).toBe('January 01, 2020')
+    })
+
+    it('should return the correctly formatted date', () => {
+      const unixTimestamp = 1721584751
+      const date = new Date(unixTimestamp * 1000)
+
+      const result = dateTime.getDayMonthYear(date)
+
+      expect(result).toBe('July 21, 2024')
     })
   })
 

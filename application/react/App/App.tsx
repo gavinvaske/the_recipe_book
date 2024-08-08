0c140e942fae175d3dc5803864b00abbb9e7d4b5
Gavin Vaske <Gavin.Vaske@gmail.com>
2024-08-07 22:45:39 -0600

    fix navbar showing up on non-auth pages

--- application/react/App/App.tsx
+++ application/react/App/App.tsx
@@ -33,41 +33,38 @@ export function App() {
       <Routes >
         <Route path='react-ui'>
 
-          {/* PUBLIC Routes without nav */}
+          {/* PUBLIC ROUTES*/}
           <Route path='login' element={<Login />}></Route>
-
-          <Route element={<TopNavbarLayout />}>
-            {/* PUBLIC Routes with nav */}
-            <Route path='*' element={<PageNotFound />} /> {/* TODO @Storm: Build this page */}
+          <Route path='*' element={<PageNotFound />} />
             
-            {/* PROTECTED ROUTES: USER / ADMIN */}
+            {/* PROTECTED ROUTES */}
             <Route element={<ProtectedRoute allowedRoles={[USER, ADMIN]}/>}>
-              
-              <Route path='' element={<Home />}></Route>
-              <Route path='inventory' element={<Inventory />}></Route>
+              <Route element={<TopNavbarLayout />}>
+                <Route path='' element={<Home />}></Route>
+                <Route path='inventory' element={<Inventory />}></Route>
 
-              <Route path='forms'>
-                <Route path='material-length-adjustment' element={<MaterialLengthAdjustmentForm />} />
-                <Route path='delivery-method/:mongooseId?' element={<DeliveryMethodForm />} />
-                <Route path='credit-term/:mongooseId?' element={<CreditTermForm />} />
-                <Route path='quote' element={<QuoteForm />} />
-                <Route path='customer/:mongooseId?' element={<CustomerForm />} />
-                <Route path="liner-type/:mongooseId?" element={<LinerTypeForm />} /> {/* TODO (6-5-2024): Enforce admin routes only render for admins */}
-                <Route path='material/:mongooseId?' element={<MaterialForm />} />
-                <Route path='adhesive-category/:mongooseId?' element={<AdhesiveCategoryForm />} />
-                <Route path='material-order/:mongooseId?' element={<MaterialOrderForm />} />
-              </Route>
+                <Route path='forms'>
+                  <Route path='material-length-adjustment' element={<MaterialLengthAdjustmentForm />} />
+                  <Route path='delivery-method/:mongooseId?' element={<DeliveryMethodForm />} />
+                  <Route path='credit-term/:mongooseId?' element={<CreditTermForm />} />
+                  <Route path='quote' element={<QuoteForm />} />
+                  <Route path='customer/:mongooseId?' element={<CustomerForm />} />
+                  <Route path="liner-type/:mongooseId?" element={<LinerTypeForm />} /> {/* TODO (6-5-2024): Enforce admin routes only render for admins */}
+                  <Route path='material/:mongooseId?' element={<MaterialForm />} />
+                  <Route path='adhesive-category/:mongooseId?' element={<AdhesiveCategoryForm />} />
+                  <Route path='material-order/:mongooseId?' element={<MaterialOrderForm />} />
+                </Route>
 
-              <Route path='tables'>
-                <Route path='credit-term' element={<CreditTermTable />} />
-                <Route path='delivery-method' element={<DeliveryMethodTable />} />
-                <Route path='liner-type' element={<LinerTypeTable />} />
-                <Route path='material' element={<MaterialTable />} />
-                <Route path='adhesive-category' element={<AdhesiveCategoryTable />} />
-                <Route path='customer' element={<CustomerTable />} />
-                <Route path='material-order' element={<MaterialOrderTable />} />
+                <Route path='tables'>
+                  <Route path='credit-term' element={<CreditTermTable />} />
+                  <Route path='delivery-method' element={<DeliveryMethodTable />} />
+                  <Route path='liner-type' element={<LinerTypeTable />} />
+                  <Route path='material' element={<MaterialTable />} />
+                  <Route path='adhesive-category' element={<AdhesiveCategoryTable />} />
+                  <Route path='customer' element={<CustomerTable />} />
+                  <Route path='material-order' element={<MaterialOrderTable />} />
+                </Route>
               </Route>
-            </Route>
           </Route>
 
         </Route>

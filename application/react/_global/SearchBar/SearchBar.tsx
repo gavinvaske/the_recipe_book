44442919a7edad519ecc406a2abec4df153d9b13
Gavin Vaske <Gavin.Vaske@gmail.com>
2024-08-10 13:34:57 -0600

    minor format change

--- application/react/_global/SearchBar/SearchBar.tsx
+++ application/react/_global/SearchBar/SearchBar.tsx
@@ -1,11 +1,11 @@
 import React, { forwardRef } from 'react'
 import './SearchBar.scss'
 
-
 type Props = {
   value: string,
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
+
 const SearchBar = forwardRef((props: Props, ref: any) => {
   const { value, onChange} = props;
 

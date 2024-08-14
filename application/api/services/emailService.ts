7ce86d005bae3ab60a4f488f948babeeed7cee93
OldRustyDayton <Russidayton73@gmail.com>
2024-08-14 15:36:06 -0500

    twilio password reset access changes

--- application/api/services/emailService.ts
+++ application/api/services/emailService.ts
@@ -4,7 +4,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY as string)
 export async function sendPasswordResetEmail(emailAddress: string, resetLink: string, minutesUntilExpiration: number) {
   const msg = {
     to: emailAddress,
-    from: 'The Label Factory <storm@labelAdvantage.com>',
+    from: 'The Label Factory <storm@thelabelfactory.com>',
     subject: 'Reset your "E.L.I" Account Password',
     text: `Click the link to reset your password (this link will expire in ${minutesUntilExpiration} minutes):\n\n\n ${resetLink}`,
   }

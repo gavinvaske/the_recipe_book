994b70d9ce984e2bbc49ac761802e64176667c99
Gavin Vaske <Gavin.Vaske@gmail.com>
2024-09-21 14:29:47 -0600

    setup Context/Provider for remembering dropdowns

--- application/react/index.jsx
+++ application/react/index.jsx
@@ -12,7 +12,7 @@ const root = createRoot(rootHtmlElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider> 
+      <AuthProvider>
         <FlashMessagePanel />
         <App />
       </AuthProvider>

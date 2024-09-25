7be9936bbcd5e6af4cbe2c87993bc2aa20914a23
Gavin Vaske <Gavin.Vaske@gmail.com>
2024-09-24 20:10:54 -0600

    wip: close dropdown on outside click

--- application/react/_global/Dropdown/Dropdown.tsx
+++ application/react/_global/Dropdown/Dropdown.tsx
@@ -1,46 +1,51 @@
-import React, { forwardRef, PropsWithChildren, useEffect, useImperativeHandle } from 'react';
+import React, { PropsWithChildren, useEffect, useImperativeHandle } from 'react';
 import './Dropdown.scss';
 import { useDropdownContext } from '../../_context/dropdownProvider';
 import { v4 as uuidv4 } from 'uuid';
-import { ClosableHTMLElement } from '../../_context/dropdownProvider';
 
 type Props = {
-  isActive?: boolean
-  classNames?: string
-}
+  isActive?: boolean;
+  classNames?: string;
+};
 
-export const Dropdown = forwardRef((props: PropsWithChildren<Props>, ref: any) => {
+export const Dropdown = (props: PropsWithChildren<Props>) => {
   const { isActive, classNames, children } = props;
   const context = useDropdownContext();
+  const [isOpen, setIsOpen] = React.useState(isActive || false);
   
-  if (!context) { throw new Error('useDropdownContext() must be used within a DropdownProvider'); }
+  if (!context) {
+    throw new Error('useDropdownContext() must be used within a DropdownProvider');
+  }
 
   const { registerDropdown, unregisterDropdown } = context;
-  const elementRef = React.useRef<ClosableHTMLElement>(null);
+  const elementRef = React.useRef(null);
 
   // Expose the close method to the parent component
-  useImperativeHandle(ref, () => ({
+  useImperativeHandle(elementRef, () => ({
     close: () => {
-      console.log('trying to close')
-      if (elementRef.current && elementRef.current.close) {
-        console.log('Dropdown closed')
-        elementRef.current.close();
+      if (isOpen && elementRef.current) {
+        setIsOpen(false);
       }
     }
   }));
 
   useEffect(() => {
+    setIsOpen(isActive || false);
+  }, [isActive]);
+
+  useEffect(() => {
     const uuid = uuidv4();
 
     registerDropdown(uuid, elementRef);
+
     return () => {
       unregisterDropdown(uuid);
     };
   }, []);
 
   return (
-    <div className={`dropdown-menu ${classNames} ${isActive ? 'active' : ''}`} ref={elementRef}>
+    <div className={`dropdown-menu ${classNames} ${isOpen ? 'active' : ''}`} ref={elementRef}>
       {children}
     </div>
-  )
-})
+  );
+};

cb220170098b8473802fb6e183fc1fd2458ac0f7
Gavin Vaske <Gavin.Vaske@gmail.com>
2025-02-26 19:11:51 -0700

    simplify modal click handlers

--- application/react/_global/FullscreenModal/FullscreenModal.tsx
+++ application/react/_global/FullscreenModal/FullscreenModal.tsx
@@ -9,18 +9,14 @@ type Props = {
 export const FullScreenModal = (props: Props) => {
   const { onClose, children } = props;
 
-  const fullScreenModalBackgroundClassName = 'fullscreen-modal-background'
-
   const closeModalIfBackgroundWasClicked = (e) => {
-    const wasBackgroundClicked = e.target.classList.contains(fullScreenModalBackgroundClassName)
-
-    if (wasBackgroundClicked){
+    if (e.target === e.currentTarget) {
       onClose();
     }
   }
 
   return (
-    <div className={`modal ${fullScreenModalBackgroundClassName}`} onClick={(e) => closeModalIfBackgroundWasClicked(e) }>
+    <div className='modal fullscreen-modal-background' onClick={(e) => closeModalIfBackgroundWasClicked(e) }>
       <div className='modal-box'>
         <i className='fa-light fa-xmark close-modal' onClick={() => onClose()}></i>
         {children}
--- application/react/_global/Modal/Modal.tsx
+++ application/react/_global/Modal/Modal.tsx
@@ -8,23 +8,20 @@ type Props = {
 
 export const Modal = (props: Props) => {
   const { onClose, children } = props;
-  const modalBackgroundClassName = 'modal-background'
 
   /* 
     VERY IMPORTANT! Otherwise clicks on this modal bubble up to an unknown number of parents. 
     If this is removed, bad stuff will happen when modals are clicked...
   */
-  const onClick = (e) => {
-    const wasBackgroundClicked = e.target.classList.contains(modalBackgroundClassName)
-
-    if (wasBackgroundClicked){
-      onClose();
-    }
-    e.stopPropagation();  // VERY IMPORTANT: Prevents this click from bubbling up to parents'
-  }
+    const handleBackgroundClick = (e) => {
+      e.stopPropagation(); // This prevents the click from reaching the parent component and triggering its own click event.
+      if (e.target === e.currentTarget) {
+        onClose();
+      }
+    };
 
   return (
-    <div className={modalBackgroundClassName} onClick={(e) => onClick(e)}>
+    <div className='modal-background' onClick={handleBackgroundClick}>
       <div className='modal-box'>
         <i className='fa-light fa-xmark close-modal' onClick={() => onClose()}></i>
         {children}

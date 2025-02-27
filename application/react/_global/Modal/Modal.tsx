5fd5cff28eb8b48877961fcba1bb49277feb065c
Gavin Vaske <Gavin.Vaske@gmail.com>
2025-02-26 19:08:56 -0700

    fix bug where clicking on modal was closing it

--- application/react/_global/Modal/Modal.tsx
+++ application/react/_global/Modal/Modal.tsx
@@ -6,22 +6,27 @@ type Props = {
   children: React.ReactNode
 }
 
-export const Modal = (props) => {
+export const Modal = (props: Props) => {
   const { onClose, children } = props;
+  const modalBackgroundClassName = 'modal-background'
 
   /* 
     VERY IMPORTANT! Otherwise clicks on this modal bubble up to an unknown number of parents. 
     If this is removed, bad stuff will happen when modals are clicked...
   */
   const onClick = (e) => {
-    onClose();
+    const wasBackgroundClicked = e.target.classList.contains(modalBackgroundClassName)
+
+    if (wasBackgroundClicked){
+      onClose();
+    }
     e.stopPropagation();  // VERY IMPORTANT: Prevents this click from bubbling up to parents'
   }
 
   return (
-    <div className='modal-background' onClick={(e) => onClick(e)}>
+    <div className={modalBackgroundClassName} onClick={(e) => onClick(e)}>
       <div className='modal-box'>
-        <i className="fa-light fa-xmark" onClick={() => onClose()}></i>
+        <i className='fa-light fa-xmark close-modal' onClick={() => onClose()}></i>
         {children}
       </div>
     </div>
